5cff40952cbc285869c75c71967f849984c16497
LAPTOP-NTEF1TGQ\tlstj <tlstj212@naver.com>
2023-09-13 17:35:42 +0900

    기록 카테고리 해당 부분 간단 css

---
+++ BodyShare-app/src/pages/analysis/index.js
@@ -0,0 +1,27 @@
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+const Analysis = function() {
+  return (
+    <>
+      {/* <Calender></Calender>
+      <Note>
+        <TitleDate>9월 7일 목</TitleDate>
+        <SportNote></SportNote>
+        <FoodNote></FoodNote>
+      </Note>
+      <NoteAdd>
+        <SportAdd>
+            <AddBt>+</AddBt>
+        </SportAdd>
+        <FoodAdd>
+            <AddBt>+</AddBt>
+        </FoodAdd>
+      </NoteAdd>
+      <Outlet /> */}
+    </>
+  );
+};
+
+
+export default Analysis;
