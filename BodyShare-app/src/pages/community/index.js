6d645157513cb2c3e93cebe821dec503dee5bb05
DESKTOP-V9UK6HC\user <csi9509@kpu.ac.kr>
2023-09-14 17:21:56 +0900

    test: 공통 코드 테스트

--- BodyShare-app/src/pages/community/index.js
+++ BodyShare-app/src/pages/community/index.js
@@ -1,4 +1,10 @@
 import styled from "styled-components";
+import Button from "../../components/commons/Button";
+import Card from "../../components/commons/Card";
+import Tag from "../../components/commons/Tag";
+import Input from "../../components/commons/Input";
+import plus from "../../assets/Img/buttonplus.png"
+import banner from "../../assets/Img/banner.jpg";
 
 const Coontainer = styled.div`
 `
@@ -6,6 +12,17 @@ const Coontainer = styled.div`
 const Community = function () {
   return (
     <Coontainer>
+      <span>
+        <Button name="추가하기" img={plus} width="200px"></Button></span>
+      <Tag tagName="끝" />
+      <Card
+        img={banner}
+        title="9/7(목)... 🧘🏻sdfasflksajflkjslkfjalkds;fjlks;djflkdsjlflksdjf"
+        contents="요가에 재미 붙였네요 :)asddgfasfsjdlkfjlskfjl;asj;fljslk;gfjslkd;fjlsdjfl;jsdf;jasjkfsfsdfsfsfsaf sfsdfsfsfs sdfsd fsd fsdf safsdfjalksgfjlksajfl;sadj lkfsjdlkfjslkd;afj;lsdlkfjs;dflkjsdlk;f"
+        tagName="필라테스"
+        footer="20203년12월31일"
+      />
+      <Input placeholder="placeholder 입니다." />
       <h2>Hi5</h2>
       <h2>Hi4</h2>
       <h2>Hi0</h2>
@@ -74,6 +91,7 @@ const Community = function () {
       <h2>Hi4</h2>
       <h2>Hi5</h2>
       <h2>Hi6</h2>
+      <Button />
     </Coontainer >
   );
 };
