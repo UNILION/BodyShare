cc5598fa37108d8acc46b87bc20c9d7791e93e02
DESKTOP-V9UK6HC\user <csi9509@kpu.ac.kr>
2023-09-14 11:21:40 +0900

    feat: 커뮤니티 index 추가

---
+++ BodyShare-app/src/pages/community/index.js
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+
+const Coontainer = styled.div`
+`
+
+const Community = function () {
+  return (
+    <Coontainer>
+      <h2>Hi5</h2>
+      <h2>Hi4</h2>
+      <h2>Hi0</h2>
+      <h2>Hi9</h2>
+      <h2>Hi8</h2>
+      <h2>Hi7</h2>
+      <h2>Hi6</h2>
+      <h2>Hi5</h2>
+      <h2>Hi4</h2>
+      <h2>Hi3</h2>
+      <h2>Hi2</h2>
+      <h2>Hi1</h2>
+      <h2>Hi4</h2>
+      <h2>Hi3</h2>
+      <h2>Hi5</h2>
+      <h2>Hi6</h2>
+      <h2>Hi0</h2>
+      <h2>Hi9</h2>
+      <h2>Hi7</h2>
+      <h2>Hi5</h2>
+      <h2>Hi3</h2>
+      <h2>Hi1</h2>
+      <h2>Hi3</h2>
+      <h2>Hi2</h2>
+      <h2>Hi3</h2>
+      <h2>Hi2</h2>
+      <h2>Hi3</h2>
+      <h2>Hi1</h2>
+      <h2>Hi3</h2>
+      <h2>Hi2</h2>
+      <h2>Hi3</h2>
+      <h2>Hi3</h2>
+      <h2>Hi2</h2>
+      <h2>Hi3</h2>
+      <h2>Hi2</h2>
+      <h2>Hi3</h2>
+      <h2>Hi2</h2>
+      <h2>Hi3</h2>
+      <h2>Hi1</h2>
+      <h2>Hi2</h2>
+      <h2>Hi3</h2>
+      <h2>Hi4</h2>
+      <h2>Hi6</h2>
+      <h2>Hi7</h2>
+      <h2>Hi0</h2>
+      <h2>Hi9</h2>
+      <h2>Hi8</h2>
+      <h2>Hi7</h2>
+      <h2>Hi6</h2>
+      <h2>Hi5</h2>
+      <h2>Hi4</h2>
+      <h2>Hi3</h2>
+      <h2>Hi2</h2>
+      <h2>Hi1</h2>
+      <h2>Hi9</h2>
+      <h2>Hi8</h2>
+      <h2>Hi7</h2>
+      <h2>Hi6</h2>
+      <h2>Hi5</h2>
+      <h2>Hi3</h2>
+      <h2>Hi4</h2>
+      <h2>Hi2</h2>
+      <h2>Hi1</h2>
+      <h2>Hi2</h2>
+      <h2>Hi3</h2>
+      <h2>Hi4</h2>
+      <h2>Hi5</h2>
+      <h2>Hi6</h2>
+    </Coontainer >
+  );
+};
+
+export default Community;
