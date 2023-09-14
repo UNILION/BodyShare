3200e3a46da77ea625a0745b177e316e5de237be
DESKTOP-V9UK6HC\user <csi9509@kpu.ac.kr>
2023-09-14 11:20:59 +0900

    feat: 겹침현상으로 margin 추가

--- BodyShare-app/src/components/layout/Header.js
+++ BodyShare-app/src/components/layout/Header.js
@@ -2,33 +2,34 @@ import styled from "styled-components";
 import logo from "../../assets/Img/logo.png"
 
 const Container = styled.div`
-    position:relative;
-    background-color: white;
-    padding-top: 20px;
+  position:relative;
+  background-color: white;
+  padding-top: 20px;
+  margin-bottom: 5px;
 
-    &:before {
+  &:before {
     content:'';
     position:absolute;
     width:100%;
     height:5px;
     background:linear-gradient(to right,  #FF798E, #556FFF);
     bottom:-5px;
-    }
+  }
 `
 
 const Logo = styled.img`
-    background: white;
-    width: 50%;
-    display: grid;
-    margin: 0 auto;
+  background: white;
+  width: 50%;
+  display: grid;
+  margin: 0 auto;
 `
 
-const Header = function() {
-    return (
-        <Container>
-        <Logo src={logo}/>
-        </Container>
-    )
+const Header = function () {
+  return (
+    <Container>
+      <Logo src={logo} />
+    </Container>
+  )
 };
 
 export default Header; 
