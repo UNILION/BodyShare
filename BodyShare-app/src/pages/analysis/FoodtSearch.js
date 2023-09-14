5c4bdcb1fafdc8ecf32c5fd0fc4399b5959ce880
LAPTOP-NTEF1TGQ\tlstj <tlstj212@naver.com>
2023-09-14 18:28:40 +0900

    feat: 음식 검색 페이지

---
+++ BodyShare-app/src/pages/analysis/FoodtSearch.js
@@ -0,0 +1,86 @@
+import styled from "styled-components";
+import search from "../../assets/Img/search.png"
+import previous from "../../assets/Img/Previous.png";
+
+const FoodtSearchContainer = styled.div`
+  display: grid;
+  grid-template-rows: auto auto auto 1fr;
+  gap: 10px;
+`;
+
+const PreviousButton = styled.img`
+    grid-row: 1;
+    width: 21px;
+    height: 21px;
+    margin-top: 10px;
+`;
+const SearchInput = styled.div`
+  grid-row: 2;
+  /* background-color: blue; */
+`;
+
+const Input = styled.input`
+  width: 300px;
+  height: 45px;
+  border: 1px solid rgba(135, 135, 135, 0.3);
+  border-radius: 15px;
+  text-align: center;
+`;
+
+const Search = styled.img`
+
+`;
+
+const FoodSearchResult = styled.div`
+  grid-row: 4;
+`;
+
+const ResultList = styled.ul`
+  list-style: none;
+  gap: 10px;
+`;
+const ResultButton = styled.button`
+    background-color: white;
+    border: none;
+    padding-top: 45px;
+    padding-bottom: 10px;
+`;
+const RP = styled.p`
+    font-size: 17px;
+`;
+
+const Line = styled.div`
+  width: 360px;
+  border: 1px solid rgba(135, 135, 135, 0.3);
+`;
+
+
+const FoodtSearch = function() {
+  return (
+    <>
+        <FoodtSearchContainer>
+            <PreviousButton src={previous}></PreviousButton>
+            <SearchInput>
+                <Search src={search}></Search>
+                <Input type="text" placeholder="찾으시는 음식을 검색해주세요"/>
+            </SearchInput>
+            <FoodSearchResult>
+                <ResultList>
+                    <ResultButton><RP>김치</RP></ResultButton>
+                    <Line></Line>
+                    <ResultButton><RP>김치</RP></ResultButton>
+                    <Line></Line>
+                    <ResultButton><RP>김치</RP></ResultButton>
+                    <Line></Line>
+                    <ResultButton><RP>김치</RP></ResultButton>
+                    <Line></Line>
+                </ResultList>
+            </FoodSearchResult>
+        </FoodtSearchContainer>
+      
+    </>
+  );
+};
+
+
+export default FoodtSearch;
