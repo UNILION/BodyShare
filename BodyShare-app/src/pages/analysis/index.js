4bdedc54c5ff3f3a0591373e1d114c3d0c10f2a7
LAPTOP-NTEF1TGQ\tlstj <tlstj212@naver.com>
2023-09-14 10:16:56 +0900

    이전버튼 이미지

--- BodyShare-app/src/pages/analysis/index.js
+++ BodyShare-app/src/pages/analysis/index.js
@@ -1,24 +1,13 @@
-import { Outlet } from "react-router-dom";
 import styled from "styled-components";
+
+
+
+
 
 const Analysis = function() {
   return (
     <>
-      {/* <Calender></Calender>
-      <Note>
-        <TitleDate>9월 7일 목</TitleDate>
-        <SportNote></SportNote>
-        <FoodNote></FoodNote>
-      </Note>
-      <NoteAdd>
-        <SportAdd>
-            <AddBt>+</AddBt>
-        </SportAdd>
-        <FoodAdd>
-            <AddBt>+</AddBt>
-        </FoodAdd>
-      </NoteAdd>
-      <Outlet /> */}
+      
     </>
   );
 };
