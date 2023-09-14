67bd56b5cb4b37b6aa801e27e678a1fb9522aaa1
LAPTOP-NTEF1TGQ\tlstj <tlstj212@naver.com>
2023-09-14 10:18:03 +0900

    분석-운동 검색 페이지

---
+++ BodyShare-app/src/pages/analysis/SportSearch.js
@@ -0,0 +1,115 @@
+import styled from "styled-components";
+import search from "../../assets/Img/search.png"
+import previous from "../../assets/Img/Previous.png";
+
+const SportSearchContainer = styled.div`
+  display: grid;
+  grid-template-rows: auto auto auto 1fr;
+  gap: 10px;
+`;
+
+const PreviousButton = styled.img`
+    grid-row: 1;
+    width: 21px;
+    height: 21px;
+    margin-top: 10px;
+`;
+const SearchInput = styled.div`
+    grid-row: 2;
+`;
+
+const Input = styled.input`
+  width: 300px;
+  height: 45px;
+  border: 1px solid rgba(135, 135, 135, 0.3);
+  border-radius: 15px;
+  text-align: center;
+`;
+
+const SportCategory = styled.div`
+  grid-row: 3;
+`;
+const Search = styled.img`
+
+`;
+
+const CategoryButton = styled.button`
+  width: 81px;
+  height: 36px;
+  border: none;
+  border-radius: 15px;
+  background-color: rgb(85, 111, 255, 0.3);
+  margin-left: 9px;
+`;
+
+const SportSearchResult = styled.div`
+  grid-row: 4;
+`;
+
+const CategoryList = styled.ul`
+  list-style: none;
+  display: flex;
+`;
+const CP = styled.p`
+    font-size: 12px;
+    font-weight: bold;
+    color: #656565;
+`;
+
+const ResultList = styled.ul`
+  list-style: none;
+  gap: 10px;
+`;
+const ResultButton = styled.button`
+    background-color: white;
+    border: none;
+    padding-top: 45px;
+    padding-bottom: 10px;
+`;
+const RP = styled.p`
+    font-size: 17px;
+`;
+
+const Line = styled.div`
+  width: 360px;
+  border: 1px solid rgba(135, 135, 135, 0.3);
+`;
+
+
+const SportSearch = function() {
+  return (
+    <>
+        <SportSearchContainer>
+            <PreviousButton src={previous}></PreviousButton>
+            <SearchInput>
+                <Search src={search}></Search>
+                <Input type="text" placeholder="찾으시는 운동을 검색해주세요"/>
+            </SearchInput>
+            <SportCategory>
+                <CategoryList>
+                    <CategoryButton><CP>전체</CP></CategoryButton>
+                    <CategoryButton><CP>전체</CP></CategoryButton>
+                    <CategoryButton><CP>전체</CP></CategoryButton>
+                    <CategoryButton><CP>전체</CP></CategoryButton>
+                </CategoryList>
+            </SportCategory>
+            <SportSearchResult>
+                <ResultList>
+                    <ResultButton><RP>수영</RP></ResultButton>
+                    <Line></Line>
+                    <ResultButton><RP>수영</RP></ResultButton>
+                    <Line></Line>
+                    <ResultButton><RP>수영</RP></ResultButton>
+                    <Line></Line>
+                    <ResultButton><RP>수영</RP></ResultButton>
+                    <Line></Line>
+                </ResultList>
+            </SportSearchResult>
+        </SportSearchContainer>
+      
+    </>
+  );
+};
+
+
+export default SportSearch;
