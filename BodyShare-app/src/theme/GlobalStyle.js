853420486adee378d0aa9dcd10e6d66f4d9cb15b
DESKTOP-V9UK6HC\user <csi9509@kpu.ac.kr>
2023-09-14 11:22:02 +0900

    refactor: 띄어쓰기 맞춤

--- BodyShare-app/src/theme/GlobalStyle.js
+++ BodyShare-app/src/theme/GlobalStyle.js
@@ -2,15 +2,15 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
-${reset}
-body {
+  ${reset}
+  body {
     @font-face {
-    font-family: 'GmarketSansMedium';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
-}
-}
+      font-family: 'GmarketSansMedium';
+      src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
+      font-weight: normal;
+      font-style: normal;
+    }
+  }
 `;
 
 export default GlobalStyle;
