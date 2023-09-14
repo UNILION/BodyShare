fea69a5b30642911bad7d637ac8004ef10adc58d
ahn <nameisb22@naver.com>
2023-09-14 14:56:04 +0900

    feat: 프로필 수정 파일 생성

---
+++ BodyShare-app/src/pages/mypage/MyProfileModify.js
@@ -0,0 +1,344 @@
+import styled from "styled-components";
+
+const All = styled.div`
+  margin-left: 3px;
+  margin-right: 3px;
+`
+
+const Backbutton = styled.button`
+  margin-top: 3px;
+  width: 20px;
+  height: 20px;
+  background-color: #D9D9D9;
+  border-radius: 20px;
+  border: none;
+  cursor: pointer;
+`
+
+const Titleo = styled.p`
+  margin-top: 7px;
+  font-size: 20px;
+`
+
+const BannerPic = styled.button`
+  margin-top: 7px;
+  padding: 10px 20px;
+  border: 1px solid #000000;
+  border-radius: 15px;
+  border-style: dashed;
+  cursor: pointer;
+  width: 372px;
+  height: 60px;
+  background-color: #FFFFFF;
+  color: #878787;
+`
+
+const TitleT = styled.p`
+  margin-top: 7px;
+  font-size: 20px;
+`
+
+const ProfilePic = styled.button`
+  margin-top: 7px;
+  padding: 10px 20px;
+  border: 1px solid #000000;
+  border-style: dashed;
+  border-radius: 15px;
+  cursor: pointer;
+  width: 372px;
+  height: 60px;
+  background-color: #FFFFFF;
+  color: #878787;
+`
+
+const Idul = styled.ul`
+  margin-top: 7px;
+  display: flex;
+`
+
+const IdTitle = styled.div`
+  font-size: 20px;
+  color: #000000;
+  border: 1px solid rgba(46, 44, 61, 0.3);
+  width: 82px;
+  height: 46px;
+  border-radius: 15px 0px 0px 15px;
+`
+
+const Id = styled.div`
+  font-size: 20px;
+  color: #777777;
+  border: 1px solid rgba(46, 44, 61, 0.3);
+  width: 284px;
+  height: 46px;
+  border-radius: 0px 15px 15px 0px;
+`
+
+const Nickul = styled.ul`
+  margin-top: 7px;
+  display: flex;
+`
+
+const NickTitle = styled.div`
+  font-size: 20px;
+  color: #000000;
+  border: 1px solid rgba(46, 44, 61, 0.3);
+  width: 82px;
+  height: 46px;
+  border-radius: 15px 0px 0px 15px;
+`
+
+const Nick = styled.p`
+  font-size: 20px;
+  color: #000000;
+  border: 1px solid rgba(46, 44, 61, 0.3);
+  width: 233px;
+  height: 46px;
+  border-radius: 0px 15px 15px 0px;
+`
+
+const Nickbutton = styled.button`
+  margin-top: 7px;
+  margin-left: 10px;
+  width: 34px;
+  height: 34px;
+  border-radius: 34px;
+  border: none;
+  cursor: pointer;
+  background-color: #556FFF;
+`
+
+const Nickwarnig = styled.p`
+  margin-top: 5px;
+  font-size: 11px;
+  color: red;
+`
+
+const Pwul = styled.ul`
+  margin-top: 7px;
+  display: flex;
+`
+
+const PwTitle = styled.div`
+  font-size: 20px;
+  color: #000000;
+  border: 1px solid rgba(46, 44, 61, 0.3);
+  width: 100px;
+  height: 46px;
+  border-radius: 15px 0px 0px 15px;
+`
+
+const Pw = styled.div`
+  font-size: 20px;
+  color: #000000;
+  border: 1px solid rgba(46, 44, 61, 0.3);
+  width: 196px;
+  height: 46px;
+  border-radius: 0px 15px 15px 0px;
+`
+
+const Pwbutton = styled.button`
+  margin-top: 7px;
+  margin-left: 10px;
+  width: 76px;
+  height: 34px;
+  border: none;
+  border-radius: 23px;
+  cursor: pointer;
+  background-color: #556FFF;
+  color: #FFFFFF;
+  font-size: 15px;
+`
+
+const Bodyul = styled.ul`
+  margin-top: 7px;
+  display: flex;
+
+`
+
+const Talltitle = styled.div`
+  font-size: 20px;
+  color: #000000;
+  border: 1px solid rgba(46, 44, 61, 0.3);
+  width: 37px;
+  height: 46px;
+  border-radius: 15px 0px 0px 15px;
+`
+
+const Tsizebox = styled.div`
+  color: #000000;
+  border: 1px solid rgba(46, 44, 61, 0.3);
+  width: 142px;
+  height: 46px; 
+  display: flex;
+`
+
+const Tallinput = styled.input`
+  border: 1px solid rgba(46, 44, 61, 0.3);
+  border-radius: 23px;
+  width: 50px;
+  height: 20px;
+`
+
+const Tallsize = styled.p`
+    font-size: 20px;
+    color: #000000;
+`
+
+const Weighttitle = styled.div`
+  color: #000000;
+  border: 1px solid rgba(46, 44, 61, 0.3);
+  width: 73px;
+  height: 46px;
+  font-size: 20px;
+`
+
+const Wsizebox = styled.div`
+  color: #000000;
+  border: 1px solid rgba(46, 44, 61, 0.3);
+  width: 114px;
+  height: 46px;
+  display: flex;
+  border-radius: 0px 15px 15px 0px;
+`
+
+const Weightinput = styled.input`
+  border: 1px solid rgba(46, 44, 61, 0.3);
+  border-radius: 23px;
+  width: 50px;
+  height: 20px;
+`
+
+const Weightsize = styled.p`
+  font-size: 20px;
+    color: #000000;
+`
+
+const Bodywarnig = styled.p`
+  margin-top: 5px;
+  font-size: 11px;
+  color: red;
+`
+
+const Cateckul = styled.div`
+  margin-top: 7px;
+  display: flex;
+`
+
+const Catetitle = styled.p`
+  font-size: 20px;
+  color: #000000;
+`
+
+const Catesearch = styled.button`
+  font-size: 15px;
+  color: #FFFFFF;
+  background-color: #556FFF;
+  border-radius: 23px;
+  width: 76px;
+  height: 31px;
+  border: none;
+  margin-left: 157px;
+`
+
+const Cateul = styled.div`
+  margin-top: 3px;
+  display: flex;
+`
+
+const Cate = styled.div`
+  border: 0px solid ;
+  background-color: rgba(85, 111, 255, 0.3);
+  border-radius: 23px;
+  width: 55px;
+  height: 26px;
+  font-size: 13px;
+  text-align: center;
+  color: #656565;
+`
+
+const Catedel = styled.button`
+  border: 0px solid ;
+  background-color: rgba(85, 111, 255, 0.3);
+  border-radius: 24.85px;
+  width: 24.85px;
+  height: 24.85px;
+  font-size: 13px;
+  text-align: center;
+  color: #FFFFFF;
+`
+
+const Catewarnig = styled.p`
+  margin-top: 5px;
+  font-size: 11px;
+  color: red;
+`
+
+const Modifyok = styled.button`
+  background-color: #556FFF; 
+    color: #fff; 
+    padding: 10px 20px;
+    border: none;
+    border-radius: 23px;
+    cursor: pointer;
+    margin-top: 16px;
+    width: 211px;
+    height: 45px;
+    float: right;
+    font-size: 20px;
+`
+
+const MyProfileModify = function () {
+  return (
+    <>
+      <All>
+        <Backbutton></Backbutton>
+        <Titleo>배너사진</Titleo>
+        <BannerPic>변경을 하고싶은 사진을 업로드 해주세요!</BannerPic>
+        <TitleT>프로필 사진</TitleT>
+        <ProfilePic>변경을 하고싶은 사진을 업로드 해주세요!</ProfilePic>
+        <Idul>
+          <IdTitle>아이디</IdTitle>
+          <Id>kimm</Id>
+        </Idul>
+        <Nickul>
+          <NickTitle>닉네임</NickTitle>
+          <Nick>kimmmmm</Nick>
+          <Nickbutton></Nickbutton>
+        </Nickul>
+        <Nickwarnig>중복된 닉네임입니다.</Nickwarnig>
+        <Pwul>
+          <PwTitle>비밀번호</PwTitle>
+          <Pw>********</Pw>
+          <Pwbutton>변경</Pwbutton>
+        </Pwul>
+        <Bodyul>
+          <Talltitle>키</Talltitle>
+          <Tsizebox>
+            <Tallinput></Tallinput>
+            <Tallsize>cm</Tallsize>
+          </Tsizebox>
+          <Weighttitle>몸무게</Weighttitle>
+          <Wsizebox>
+            <Weightinput></Weightinput>
+            <Weightsize>kg</Weightsize>
+          </Wsizebox>
+        </Bodyul>
+        <Bodywarnig>필수로 작성해주세요!</Bodywarnig>
+        <Cateckul>
+          <Catetitle>나의 관심 카테고리</Catetitle>
+          <Catesearch>검색</Catesearch>
+        </Cateckul>
+        <Cateul>
+          <Cate>요가</Cate>
+          <Catedel>X</Catedel>
+        </Cateul>
+        <Catewarnig>한 개 이상의 카테고리를 선택해주세요!</Catewarnig>
+        <Modifyok>프로필 수정완료</Modifyok>
+
+      </All>
+    </>
+  )
+};
+
+export default MyProfileModify;
