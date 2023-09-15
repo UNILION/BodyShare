49d0a2aaf83ede6bc89df17bccb70156d911b490
DESKTOP-V9UK6HC\user <csi9509@kpu.ac.kr>
2023-09-16 02:31:58 +0900

    Feat: 카테고리 선택 페이지 추가

---
+++ BodyShare-app/src/pages/community/commu/CommuCategory.js
@@ -0,0 +1,124 @@
+import styled from "styled-components";
+import search from "../../../assets/Img/search.png";
+import backButton from "../../../assets/Img/back.png";
+import plus from "../../../assets/Img/buttonplus.png"
+import Button from "../../../components/commons/Button"
+import { useNavigate } from "react-router-dom";
+import Tag from "../../../components/commons/Tag";
+
+const Container = styled.div`
+  padding: 10px 20px;
+`;
+
+const PreviousButton = styled.button`
+  margin-top: 3px;
+  grid-row: 1;
+  width: 20px;
+  height: 20px;
+  background-image: url(${backButton});
+  background-size: cover;
+  border-radius: 20px;
+  border: none;
+  cursor: pointer;
+`;
+
+const SearchInput = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  margin-bottom: 20px;
+`;
+
+const Input = styled.input`
+  width: 260px;
+  height: 45px;
+  border: 1px solid rgba(135, 135, 135, 0.3);
+  border-radius: 15px;
+  text-align: center;
+  padding: 0 20px;
+`;
+
+const Search = styled.img`
+  margin: auto;
+`;
+
+const CategoryList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+`;
+
+const ResultList = styled.div`
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+`;
+
+const ResultButton = styled.button`
+  background-color: white;
+  border: none;
+  padding-top: 55px;
+  cursor: pointer;
+`;
+
+const RP = styled.p`
+  font-size: 17px;
+  text-align: left;
+  margin-bottom: 10px;
+`;
+
+const Line = styled.div`
+  width: 360px;
+  border: 1px solid rgba(135, 135, 135, 0.3);
+`;
+
+const CommuSearch = function () {
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <PreviousButton onClick={() => navigate("/community")} />
+      <SearchInput>
+        <Search src={search} />
+        <Input type="text" placeholder="찾으시는 운동을 검색해주세요" />
+      </SearchInput>
+      <CategoryList>
+        <Tag tagName="전체" width="80px" height="36px" br="13px" />
+        <Tag tagName="유산소" width="80px" height="36px" br="13px" />
+        <Tag tagName="무산소" width="80px" height="36px" br="13px" />
+        <Tag tagName="근력" width="80px" height="36px" br="13px" />
+      </CategoryList>
+      <ResultList>
+        <ResultButton>
+          <RP>축구</RP>
+          <Line></Line>
+        </ResultButton>
+        <ResultButton>
+          <RP>수영</RP>
+          <Line></Line>
+        </ResultButton>
+        <ResultButton>
+          <RP>달리기</RP>
+          <Line></Line>
+        </ResultButton>
+        <ResultButton>
+          <RP>필라테스</RP>
+          <Line></Line>
+        </ResultButton>
+        <ResultButton>
+          <RP>요가</RP>
+          <Line></Line>
+        </ResultButton>
+      </ResultList>
+
+      <Button
+        name="선택하기"
+        img={plus}
+        width="200px"
+        display="block"
+        ml="auto"
+        mt="30px"
+        onClick={() => navigate("/community/communityAdd")}
+      />
+    </Container>
+  );
+};
+
+export default CommuSearch;
