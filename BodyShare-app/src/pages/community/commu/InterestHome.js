e473184d07e97aec7ded914633531b1b8fa33941
DESKTOP-V9UK6HC\user <csi9509@kpu.ac.kr>
2023-09-16 00:27:56 +0900

    feat: 세부 글자 수정 및 커뮤니티 세부이동 추가

--- BodyShare-app/src/pages/community/commu/InterestHome.js
+++ BodyShare-app/src/pages/community/commu/InterestHome.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 import Pagination from "react-js-pagination";
 import "../../../assets/css/Pagination.css";
 import Card from "../../../components/commons/Card";
-import Image1 from "../../../assets/Img/card_image1.png";
-import Image2 from "../../../assets/Img/card_image2.png";
-import Image3 from "../../../assets/Img/card_image3.png";
-import Image4 from "../../../assets/Img/card_image4.png";
+import Image1 from "../../../assets/Img/Climing1.jpg";
+import Image2 from "../../../assets/Img/Climing2.jpg";
+import Image3 from "../../../assets/Img/Climing3.jpg";
+import Image4 from "../../../assets/Img/Climing4.jpg";
 import { useState } from "react";
 
 const Group = styled.div`
@@ -16,6 +17,7 @@ const Group = styled.div`
 
 const InterestHome = function () {
   const [page, setPage] = useState(1);
+  const navigate = useNavigate();
 
   const handleChange = (page) => {
     setPage(page);
@@ -26,34 +28,38 @@ const InterestHome = function () {
       <Group>
         <Card
           img={Image1}
-          title="관심사 요가 커뮤니티"
-          contents="요가에 재미를 붙이셨네요 :)"
-          tagName="요가"
-          footer="96명의 회원이 가입함"
+          title="클라이밍 좋아하는 모임"
+          contents="클라이밍 좋아하는 사람들 모두 모두 모여라"
+          tagName="클라이밍"
+          footer="956명의 회원이 가입함"
+          onClick={() => navigate("/community/in")} 
         />
         <Card
           img={Image2}
-          title="클라이밍 커뮤니티"
-          contents="클라이밍에 재미를 붙이셨네요 :)"
+          title="대전 클라이밍"
+          contents="대전에서 클라이밍 하실 분?"
           tagName="클라이밍"
-          footer="196명의 회원이 가입함"
+          footer="543명의 회원이 가입함"
+          onClick={() => navigate("/community/in")} 
         />
       </Group>
 
       <Group>
         <Card
           img={Image3}
-          title="필라테스 커뮤니티"
-          contents="필라테스에 재미를 붙이셨네요 :)"
-          tagName="필라테스"
-          footer="153명의 회원이 가입함"
+          title="수도권 클라이밍 모임"
+          contents="수도권 사람들만 오세요:)"
+          tagName="클라이밍"
+          footer="321명의 회원이 가입함"
+          onClick={() => navigate("/community/in")} 
         />
         <Card
           img={Image4}
-          title="축구 커뮤니티"
-          contents="축구에 재미를 붙이셨네요 :)"
-          tagName="축구"
-          footer="3563명의 회원이 가입함"
+          title="클라이밍 초보들만"
+          contents="클라이밍에 재미를 붙이셨네요 :)"
+          tagName="클라이밍"
+          footer="123명의 회원이 가입함"
+          onClick={() => navigate("/community/in")} 
         />
       </Group>
       <Pagination
