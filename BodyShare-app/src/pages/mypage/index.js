e0944bf7866208bfb167b875839150f4d9d489f6
Dongyun <kj77181@gmail.com>
2023-09-15 14:00:21 +0900

    feat: index.js 연결 설정

--- BodyShare-app/src/pages/mypage/index.js
+++ BodyShare-app/src/pages/mypage/index.js
@@ -1,9 +1,9 @@
-import { Outlet } from "react-router-dom";
+import MyPage from "./MyPage.js";
 
 const MyMain = function() {
   return (
     <>
-      <Outlet />
+      <MyPage />
     </>
   );
 };
