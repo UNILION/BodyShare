157f2db89b4a0e5e5327b0e2ed6cb964d19aa64e
LAPTOP-NTEF1TGQ\tlstj <tlstj212@naver.com>
2023-09-15 23:46:48 +0900

    feat: 분석페이지- 일주일 운동 시간 비교, 칼로리 계산(운동으로 소비한 칼로리 - 섭취한 칼로리) 가로 막대 그래프 

--- BodyShare-app/src/pages/analysis/sportchart/SportChart.js
+++ BodyShare-app/src/pages/analysis/sportchart/SportChart.js
@@ -3,8 +3,6 @@ import styled from 'styled-components';
 import { Chart } from 'react-google-charts';
 
 const SliderContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 100%);
   overflow: hidden;
   width: 360px;
   margin: 35px auto;
@@ -16,18 +14,6 @@ const Slide = styled.div`
   place-items: center;
   transition: transform 0.3s ease;
   grid-column: span 1;
-`;
-
-const VerticalChartWrapper = styled.div`
-  width: 360px;
-  height: 476px;
-  background-color: black;
-`;
-
-const HorizontalChartWrapper = styled.div`
-  width: 360px;
-  height: 476px;
-  background-color: red;
 `;
 
 const Slider = styled.div`
@@ -54,8 +40,21 @@ const Button = styled.button`
 `;
 
 // 차트 스타일
-const ChartBox = styled.div`
-  grid-row: 2;
+const ChartBox1 = styled.div`
+  width: 350px;
+  height: 476px;
+  border-radius: 30px;
+  background-color: #ffffff;
+  border: 1px solid rgba(0, 0, 0, 0.25);
+  margin: 0 auto;
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ChartBox2 = styled.div`
   width: 350px;
   height: 476px;
   border-radius: 30px;
@@ -76,6 +75,9 @@ const ChartContainer1 = styled.div`
   border-radius: 15px;
   border: 1px solid rgba(135, 135, 135, 0.3);
   cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const ChartContainer2 = styled.div`
@@ -118,27 +120,22 @@ const SportChart = function () {
   };
 
   const chartData2 = [
-    ["", "운동 분"],
-    ["월", 60],
-    ["화", 45],
-    ["수", 30],
-    ["목", 75],
-    ["금", 90],
-    ["토", 120],
-    ["일", 60],
+    ["Task", "운동칼로리", "섭취칼로리"],
+    ["월", 8, 2],
+    ["화", 2, 1],
+    ["수", 4, 3],
+    ["목", 10, 5],
+    ["금", 10, 6],
+    ["토", 10, 4],
+    ["일", 10, 7]
   ];
   
   const chartOptions2 = {
-    legend: { position: "none" },
-    chart: {
-      title: "운동 분",
-    },
-    hAxis: {
-      title: "분",
-    },
-    vAxis: {
-      title: "",
-    },
+    title: "칼로리 계산(운동으로 소비한 칼로리 - 섭취한 칼로리)",
+    legend: { position: "top" },
+    hAxis: { title: "칼로리" },
+    vAxis: { title: "요일" }, 
+    bars: "horizontal", 
   };
 
   return (
@@ -146,36 +143,32 @@ const SportChart = function () {
       <SliderContainer>
         <Slider slideIndex={slideIndex}>
           <Slide>
-            <VerticalChartWrapper>
-              <ChartBox>
+              <ChartBox1>
                 <ChartContainer1>
                   <Chart
                     chartType="Bar"
-                    width="350px"
-                    height="476px"
+                    width="300px"
+                    height="460px"
                     data={chartData1}
                     options={chartOptions1}
-                    graph_id="barchart2"
+                    graph_id="barchart1"
                   />
                 </ChartContainer1>
-              </ChartBox>
-            </VerticalChartWrapper>
+              </ChartBox1>
           </Slide>
           <Slide>
-            <HorizontalChartWrapper>
-              <ChartBox>
+              <ChartBox2>
                 <ChartContainer2>
-                  <Chart
-                    chartType="BarChart" 
+                <Chart
+                    chartType="BarChart"
                     width="350px"
                     height="476px"
                     data={chartData2}
-                    options={{chartOptions2, orientation: 'horizontal'}}
+                    options={chartOptions2}
                     graph_id="barchart2"
-                  />
+                />
                 </ChartContainer2>
-              </ChartBox>
-            </HorizontalChartWrapper>
+              </ChartBox2>
           </Slide>
         </Slider>
       </SliderContainer>
