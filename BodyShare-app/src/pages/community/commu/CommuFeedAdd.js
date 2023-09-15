d7659d6b07ffe09dad1b303738ffe8745ed931e7
DESKTOP-V9UK6HC\user <csi9509@kpu.ac.kr>
2023-09-16 00:30:11 +0900

    feat: 피드 추가 페이지 생성

---
+++ BodyShare-app/src/pages/community/commu/CommuFeedAdd.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import previous from "../../../assets/Img/Previous.png"
+
+const CommuAddFeed = function () {
+  return(
+    <>
+    
+    </>
+  );
+};
+
+export default CommuAddFeed;
