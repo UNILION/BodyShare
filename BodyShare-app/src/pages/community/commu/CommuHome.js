4583e4116157219ac2bb678123f1d5390a7047ba
DESKTOP-V9UK6HC\user <csi9509@kpu.ac.kr>
2023-09-15 16:18:29 +0900

    feat: 커뮤니티 탭 메인 화면

--- BodyShare-app/src/pages/community/commu/CommuHome.js
+++ BodyShare-app/src/pages/community/commu/CommuHome.js
@@ -1,71 +1,102 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
-import Pagination from "react-js-pagination";
-import "../../../assets/css/Pagination.css";
-import Card from "../../../components/commons/Card";
-import Image1 from "../../../assets/Img/card_image1.png";
-import Image2 from "../../../assets/Img/card_image2.png";
-import Image3 from "../../../assets/Img/card_image3.png";
-import Image4 from "../../../assets/Img/card_image4.png";
-import { useState } from "react";
+import InterestHome from "./InterestHome"
+import PopularHome from "./PopularHome"
+import Search from "../../../assets/Img/buttonsearch.png"
+import Plus from "../../../assets/Img/buttonplus.png"
 
-const Group = styled.div`
- display: grid;
- grid-template-columns: 1fr 1fr;
- margin-top: 10px;
+const Container = styled.div``;
+
+const Tab = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 2fr;
+  width: 100%;
+  margin: 20px 0 15px 0;
+  cursor: pointer;
+  font-weight: bold;
 `;
 
-const CommuHome = function () {
-  const [page, setPage] = useState(1);
+const Tab1 = styled.div`
+  display: grid;
+  place-items: center;
+  width: 100%;
+  border-radius: 23px 23px 0 0;
+  background-color: ${(props) => props.bg};
+  border-bottom: ${(props) => props.under};
+`;
 
-  const handleChange = (page) => {
-    setPage(page);
+const Tab2 = styled.div`
+  display: grid;
+  place-items: center;
+  margin: 0 auto;
+  width: 100%;
+  border-radius: 23px 23px 0 0;
+  background-color: ${(props) => props.bg};
+  border-bottom: ${(props) => props.under};
+`;
+
+const Text = styled.span`
+  color: ${(props) => props.color};
+  margin-bottom: 3px;
+  font-weight: bolder;
+
+  &:hover{
+    color: #556FFF;
   }
+`;
+
+const Img = styled.img`
+  display: grid;
+  align-items: end;
+  margin-left: auto;
+  width: 50px;
+  height: 50px;
+`
+
+
+const CommuHome = function () {
+  const [Interest, setInterest] = useState(true);
+  const [Popular, setPopular] = useState(false);
+
+  const showInterest = async () => {
+    setInterest(true);
+    setPopular(false);
+  };
+
+  const showPopular = async () => {
+    setInterest(false);
+    setPopular(true);
+  };
+
+  useEffect(() => {
+    showInterest();
+  }, []);
 
   return (
-    <>
-      <Group>
-        <Card
-          img={Image1}
-          title="요가 커뮤니티"
-          contents="요가에 재미를 붙이셨네요 :)"
-          tagName="요가"
-          footer="96명의 회원이 가입함"
-        />
-        <Card
-          img={Image2}
-          title="클라이밍 커뮤니티"
-          contents="클라이밍에 재미를 붙이셨네요 :)"
-          tagName="클라이밍"
-          footer="196명의 회원이 가입함"
-        />
-      </Group>
-
-      <Group>
-        <Card
-          img={Image3}
-          title="필라테스 커뮤니티"
-          contents="필라테스에 재미를 붙이셨네요 :)"
-          tagName="필라테스"
-          footer="153명의 회원이 가입함"
-        />
-        <Card
-          img={Image4}
-          title="축구 커뮤니티"
-          contents="축구에 재미를 붙이셨네요 :)"
-          tagName="축구"
-          footer="3563명의 회원이 가입함"
-        />
-      </Group>
-      <Pagination
-        activePage={page}
-        itemsCountPerPage={4}
-        totalItemsCount={24}
-        pageRangeDisplayed={5}
-        prevPageText={"‹"}
-        nextPageText={"›"}
-        onChange={handleChange}
-      />
-    </>
+    <Container>
+      <Tab>
+        <Tab1
+          onClick={showInterest}
+          under={Interest ? "3px solid rgba(85, 111, 255,0.5)" : ""}
+        >
+          <Text color={Interest ? "#556FFF" : "rgba(0,0,0,0.2)"}>관심사 추천</Text>
+        </Tab1>
+        <Tab2
+          onClick={showPopular}
+          under={Popular ? "3px solid rgba(85, 111, 255,0.5)" : ""}
+        >
+          <Text
+            color={Popular ? "#556FFF" : "rgba(0,0,0,0.2)"}
+          >
+            인기 추천
+          </Text>
+        </Tab2>
+        <Img src={Search} />
+      </Tab>
+      {Interest ? <InterestHome /> : null}
+      {Popular ? <PopularHome /> : null}
+        <Img src={Plus} />
+    </Container>
   );
 };
 
