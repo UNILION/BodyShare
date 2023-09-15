138713258ea75f8c84439f252c3bf7cd3242f58c
DESKTOP-V9UK6HC\user <csi9509@kpu.ac.kr>
2023-09-16 00:27:03 +0900

    refactor: community - my 세부 글자 수정

--- BodyShare-app/src/pages/community/my/My.js
+++ BodyShare-app/src/pages/community/my/My.js
@@ -3,6 +3,9 @@ import Pagination from "react-js-pagination";
 import { useState } from "react";
 import "../../../assets/css/Pagination.css";
 import user from "../../../assets/Img/user.png"
+import user2 from "../../../assets/Img/card_image1.png"
+import user3 from "../../../assets/Img/card_image2.png"
+import user4 from "../../../assets/Img/card_image3.png"
 
 
 const Text = styled.div`
@@ -28,9 +31,13 @@ const Register = styled.div`
 `
 
 const Title = styled.div`
+  width: 150px;
   font-size: 18px;
   font-weight: bold;
   margin-top: 20px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;  /* 말줄임 적용 */
 `
 
 const Member = styled.div`
@@ -50,11 +57,10 @@ const Mini = styled.div`
   grid-template-columns: 2fr 1fr;
   font-weight: bold;
   cursor: pointer;
-
 `
 
 const MiniTitle = styled.span`
-    white-space: nowrap;
+  white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;  /* 말줄임 적용 */
 `
@@ -82,20 +88,20 @@ const My = function () {
       <Group>
         <Img src={user} />
         <Register>
-          <Title>클라이밍 클럽</Title>
+          <Title>헬스 클럽</Title>
           <Member>96명의 회원</Member>
           <Writing>
             <Mini>
               <MiniTitle>오늘 목표 성공</MiniTitle>
-              <MiniMember>클라이밍</MiniMember>
+              <MiniMember>헬스녀</MiniMember>
             </Mini>
             <Mini>
               <MiniTitle>오늘도 화이팅</MiniTitle>
-              <MiniMember>닉네임</MiniMember>
+              <MiniMember>헬스남</MiniMember>
             </Mini>
             <Mini>
-              <MiniTitle>새글 제목 입니다</MiniTitle>
-              <MiniMember>입니다.</MiniMember>
+              <MiniTitle>오늘의점심</MiniTitle>
+              <MiniMember>1일1운동</MiniMember>
             </Mini>
           </Writing>
         </Register>
@@ -104,22 +110,22 @@ const My = function () {
       <Hr />
 
       <Group>
-        <Img src={user} />
+        <Img src={user2} />
         <Register>
-          <Title>클라이밍 클럽</Title>
-          <Member>96명의 회원</Member>
+          <Title>요가</Title>
+          <Member>243명의 회원</Member>
           <Writing>
             <Mini>
-              <MiniTitle>오늘 목표 성공</MiniTitle>
-              <MiniMember>클라이밍</MiniMember>
+              <MiniTitle>고양이자세</MiniTitle>
+              <MiniMember>요가 강사</MiniMember>
             </Mini>
             <Mini>
-              <MiniTitle>오늘도 화이팅</MiniTitle>
-              <MiniMember>닉네임</MiniMember>
+              <MiniTitle>풀 시퀀스 요가</MiniTitle>
+              <MiniMember>자세봐주는사람</MiniMember>
             </Mini>
             <Mini>
-              <MiniTitle>새글 제목 입니다</MiniTitle>
-              <MiniMember>입니다.</MiniMember>
+              <MiniTitle>차근차근 요가</MiniTitle>
+              <MiniMember>슬로우</MiniMember>
             </Mini>
           </Writing>
         </Register>
@@ -128,22 +134,22 @@ const My = function () {
       <Hr />
 
       <Group>
-        <Img src={user} />
+        <Img src={user3} />
         <Register>
-          <Title>클라이밍 클럽</Title>
-          <Member>96명의 회원</Member>
+          <Title>클라이밍 동아리</Title>
+          <Member>152명의 회원</Member>
           <Writing>
             <Mini>
-              <MiniTitle>오늘 목표 성공</MiniTitle>
+              <MiniTitle>오픈 그립</MiniTitle>
               <MiniMember>클라이밍</MiniMember>
             </Mini>
             <Mini>
-              <MiniTitle>오늘도 화이팅</MiniTitle>
-              <MiniMember>닉네임</MiniMember>
+              <MiniTitle>쉘프린트</MiniTitle>
+              <MiniMember>볼더</MiniMember>
             </Mini>
             <Mini>
-              <MiniTitle>새글 제목 입니다</MiniTitle>
-              <MiniMember>입니다.</MiniMember>
+              <MiniTitle>핀치</MiniTitle>
+              <MiniMember>더클라임</MiniMember>
             </Mini>
           </Writing>
         </Register>
@@ -152,22 +158,22 @@ const My = function () {
       <Hr />
 
       <Group>
-        <Img src={user} />
+        <Img src={user4} />
         <Register>
-          <Title>클라이밍 클럽</Title>
-          <Member>96명의 회원</Member>
+          <Title>필라테스를 사랑하는 모임</Title>
+          <Member>194명의 회원</Member>
           <Writing>
             <Mini>
-              <MiniTitle>오늘 목표 성공</MiniTitle>
-              <MiniMember>클라이밍</MiniMember>
+              <MiniTitle>C컬 롱라인</MiniTitle>
+              <MiniMember>위필</MiniMember>
             </Mini>
             <Mini>
-              <MiniTitle>오늘도 화이팅</MiniTitle>
-              <MiniMember>닉네임</MiniMember>
+              <MiniTitle>골반 세팅법</MiniTitle>
+              <MiniMember>클래식</MiniMember>
             </Mini>
             <Mini>
-              <MiniTitle>새글 제목 입니다</MiniTitle>
-              <MiniMember>입니다.</MiniMember>
+              <MiniTitle>힐링필라테스</MiniTitle>
+              <MiniMember>힐러</MiniMember>
             </Mini>
           </Writing>
         </Register>
