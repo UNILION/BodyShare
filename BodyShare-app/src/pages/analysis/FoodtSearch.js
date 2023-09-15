3775cb56e07b4912ae5689bdc268bbd97db132c3
LAPTOP-NTEF1TGQ\tlstj <tlstj212@naver.com>
2023-09-15 20:17:25 +0900

    feat: 분석페이지 - 음식 검색 페이지 수정

--- BodyShare-app/src/pages/analysis/FoodtSearch.js
+++ BodyShare-app/src/pages/analysis/FoodtSearch.js
@@ -1,7 +1,9 @@
-import styled from "styled-components";
-import search from "../../assets/Img/search.png"
-import previous from "../../assets/Img/Previous.png";
-import { Link, useNavigate } from "react-router-dom";
+import React from 'react';
+import styled from 'styled-components';
+import search from '../../assets/Img/search.png';
+import previous from '../../assets/Img/Previous.png';
+import { useNavigate } from 'react-router-dom';
+import "../../assets/Img/buttonsearch.png";
 
 const FoodtSearchContainer = styled.div`
   display: grid;
@@ -16,12 +18,12 @@ const PreviousButton = styled.img`
   margin-top: 10px;
 
   &:hover {
-  cursor: pointer;
+    cursor: pointer;
   }
 `;
+
 const SearchInput = styled.div`
   grid-row: 2;
-  /* background-color: blue; */
 `;
 
 const Input = styled.input`
@@ -32,9 +34,7 @@ const Input = styled.input`
   text-align: center;
 `;
 
-const Search = styled.img`
-
-`;
+const Search = styled.img``;
 
 const FoodSearchResult = styled.div`
   grid-row: 4;
@@ -44,14 +44,16 @@ const ResultList = styled.ul`
   list-style: none;
   gap: 10px;
 `;
+
 const ResultButton = styled.button`
-    background-color: white;
-    border: none;
-    padding-top: 45px;
-    padding-bottom: 10px;
+  background-color: white;
+  border: none;
+  padding-top: 45px;
+  padding-bottom: 10px;
 `;
+
 const RP = styled.p`
-    font-size: 17px;
+  font-size: 17px;
 `;
 
 const Line = styled.div`
@@ -59,35 +61,98 @@ const Line = styled.div`
   border: 1px solid rgba(135, 135, 135, 0.3);
 `;
 
+// 공통 버튼 css
+const StyledButton = styled.button`
+  width: ${(props) => (props.width ? props.width : '200px')};
+  height: ${(props) => (props.height ? props.height : '45px')};
+  color: ${(props) => (props.color ? props.color : 'white')};
+  background-color: ${(props) => (props.bc ? props.bc : '#556FFF')};
+  font-size: ${(props) => (props.fs ? props.fs : '20px')};
+  border-radius: ${(props) => (props.br ? props.br : '23px')};
+  border: ${(props) =>
+    props.border ? props.border : '1px solid rgba(135, 135, 135, 0.3)'};
+  margin-top: ${(props) => (props.mt ? props.mt : '0px')};
+  margin-right: ${(props) => (props.mr ? props.mr : '0px')};
+  margin-bottom: ${(props) => (props.mb ? props.mb : '0px')};
+  margin-left: ${(props) => (props.ml ? props.ml : '0px')};
+  cursor: pointer;
 
-const FoodtSearch = function() {
+  &:hover {
+    background-color: ${(props) => (props.hoverColor ? props.hoverColor : '#2748FF')};
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+`;
+
+const Name = styled.span`
+  margin-top: ${(props) => (props.nmt ? props.nmt : 'auto')};
+  margin-right: ${(props) => (props.nmr ? props.nmr : 'auto')};
+  margin-bottom: ${(props) => (props.nmb ? props.nmb : 'auto')};
+  margin-left: ${(props) => (props.nml ? props.nml : 'auto')};
+`;
+
+const Image = styled.img`
+  width: ${(props) => (props.iwidth ? props.iwidth : '30px')};
+  height: ${(props) => (props.iheight ? props.iheight : '30px')};
+  margin-top: ${(props) => (props.imt ? props.imt : 'auto')};
+  margin-right: ${(props) => (props.imr ? props.imr : 'auto')};
+  margin-bottom: ${(props) => (props.imb ? props.imb : 'auto')};
+  margin-left: ${(props) => (props.iml ? props.iml : 'auto')};
+  display: ${(props) => (props.img ? 'grid' : 'none')};
+  place-items: center;
+`;
+
+const FoodtSearch = function () {
   const navigate = useNavigate();
 
   return (
     <>
-        <FoodtSearchContainer>
-            <PreviousButton src={previous} onClick={() => navigate("/analysis")}></PreviousButton>
-            <SearchInput>
-                <Search src={search}></Search>
-                <Input type="text" placeholder="찾으시는 음식을 검색해주세요"/>
-            </SearchInput>
-            <FoodSearchResult>
-                <ResultList>
-                    <ResultButton><RP>김치</RP></ResultButton>
-                    <Line></Line>
-                    <ResultButton><RP>김치</RP></ResultButton>
-                    <Line></Line>
-                    <ResultButton><RP>김치</RP></ResultButton>
-                    <Line></Line>
-                    <ResultButton><RP>김치</RP></ResultButton>
-                    <Line></Line>
-                </ResultList>
-            </FoodSearchResult>
-        </FoodtSearchContainer>
-      
+      <FoodtSearchContainer>
+        <PreviousButton src={previous} onClick={() => navigate('/analysis')} />
+        <SearchInput>
+          <Search src={search} />
+          <Input type="text" placeholder="찾으시는 음식을 검색해주세요" />
+        </SearchInput>
+        <FoodSearchResult>
+          <ResultList>
+            <ResultButton>
+              <RP>김치</RP>
+            </ResultButton>
+            <Line></Line>
+            <ResultButton>
+              <RP>김치</RP>
+            </ResultButton>
+            <Line></Line>
+            <ResultButton>
+              <RP>김치</RP>
+            </ResultButton>
+            <Line></Line>
+            <ResultButton>
+              <RP>김치</RP>
+            </ResultButton>
+            <Line></Line>
+          </ResultList>
+        </FoodSearchResult>
+        <StyledButton
+          width="200px"
+          height="45px"
+          color="white"
+          bc="#556FFF"
+          fs="20px"
+          br="23px"
+          border="1px solid rgba(135,135,135,0.3)"
+        >
+          <ButtonContainer>
+            <Name>Button Name</Name>
+            <Image iwidth="30px" iheight="30px" img={true} />
+          </ButtonContainer>
+        </StyledButton>
+      </FoodtSearchContainer>
     </>
   );
 };
-
 
 export default FoodtSearch;
