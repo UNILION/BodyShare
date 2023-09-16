232c32ff1d7b9b9c89948a2903b14dbd6a6072e1
JuneBeomJang <csi9509@kpu.ac.kr>
2023-09-16 11:43:19 +0900

    faet: 글 누를 시 알맞은 페이지 이동

--- BodyShare-app/src/pages/community/my/My.js
+++ BodyShare-app/src/pages/community/my/My.js
@@ -6,6 +6,7 @@ import user from "../../../assets/Img/user.png"
 import user2 from "../../../assets/Img/card_image1.png"
 import user3 from "../../../assets/Img/card_image2.png"
 import user4 from "../../../assets/Img/card_image3.png"
+import { useNavigate } from "react-router-dom";
 
 
 const Text = styled.div`
@@ -38,6 +39,7 @@ const Title = styled.div`
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;  /* 말줄임 적용 */
+  cursor: pointer;
 `
 
 const Member = styled.div`
@@ -56,13 +58,13 @@ const Mini = styled.div`
   display: grid;
   grid-template-columns: 2fr 1fr;
   font-weight: bold;
-  cursor: pointer;
 `
 
 const MiniTitle = styled.span`
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;  /* 말줄임 적용 */
+  cursor: pointer;
 `
 
 const MiniMember = styled.span`
@@ -77,6 +79,7 @@ const Hr = styled.hr`
 
 const My = function () {
   const [page, setPage] = useState(1);
+  const navigate = useNavigate();
 
   const handleChange = (page) => {
     setPage(page);
@@ -88,19 +91,19 @@ const My = function () {
       <Group>
         <Img src={user} />
         <Register>
-          <Title>헬스 클럽</Title>
+          <Title onClick={() => navigate("/community/commuIn")}>헬스 클럽</Title>
           <Member>96명의 회원</Member>
           <Writing>
             <Mini>
-              <MiniTitle>오늘 목표 성공</MiniTitle>
+              <MiniTitle onClick={() => navigate("/community/feed")}>오늘 목표 성공</MiniTitle>
               <MiniMember>헬스녀</MiniMember>
             </Mini>
             <Mini>
-              <MiniTitle>오늘도 화이팅</MiniTitle>
+              <MiniTitle onClick={() => navigate("/community/feed")}>오늘도 화이팅</MiniTitle>
               <MiniMember>헬스남</MiniMember>
             </Mini>
             <Mini>
-              <MiniTitle>오늘의점심</MiniTitle>
+              <MiniTitle onClick={() => navigate("/community/feed")}>오늘의점심</MiniTitle>
               <MiniMember>1일1운동</MiniMember>
             </Mini>
           </Writing>
@@ -112,19 +115,19 @@ const My = function () {
       <Group>
         <Img src={user2} />
         <Register>
-          <Title>요가</Title>
+          <Title onClick={() => navigate("/community/commuIn")}>요가</Title>
           <Member>243명의 회원</Member>
           <Writing>
             <Mini>
-              <MiniTitle>고양이자세</MiniTitle>
+              <MiniTitle onClick={() => navigate("/community/feed")}>고양이자세</MiniTitle>
               <MiniMember>요가 강사</MiniMember>
             </Mini>
             <Mini>
-              <MiniTitle>풀 시퀀스 요가</MiniTitle>
+              <MiniTitle onClick={() => navigate("/community/feed")}>풀 시퀀스 요가</MiniTitle>
               <MiniMember>자세봐주는사람</MiniMember>
             </Mini>
             <Mini>
-              <MiniTitle>차근차근 요가</MiniTitle>
+              <MiniTitle> onClick={() => navigate("/community/feed")}차근차근 요가</MiniTitle>
               <MiniMember>슬로우</MiniMember>
             </Mini>
           </Writing>
@@ -136,19 +139,19 @@ const My = function () {
       <Group>
         <Img src={user3} />
         <Register>
-          <Title>클라이밍 동아리</Title>
+          <Title onClick={() => navigate("/community/commuIn")}>클라이밍 동아리</Title>
           <Member>152명의 회원</Member>
           <Writing>
             <Mini>
-              <MiniTitle>오픈 그립</MiniTitle>
+              <MiniTitle onClick={() => navigate("/community/feed")}>오픈 그립</MiniTitle>
               <MiniMember>클라이밍</MiniMember>
             </Mini>
             <Mini>
-              <MiniTitle>쉘프린트</MiniTitle>
+              <MiniTitle onClick={() => navigate("/community/feed")}>쉘프린트</MiniTitle>
               <MiniMember>볼더</MiniMember>
             </Mini>
             <Mini>
-              <MiniTitle>핀치</MiniTitle>
+              <MiniTitle onClick={() => navigate("/community/feed")}>핀치</MiniTitle>
               <MiniMember>더클라임</MiniMember>
             </Mini>
           </Writing>
@@ -160,19 +163,19 @@ const My = function () {
       <Group>
         <Img src={user4} />
         <Register>
-          <Title>필라테스를 사랑하는 모임</Title>
+          <Title onClick={() => navigate("/community/commuIn")}>필라테스를 사랑하는 모임</Title>
           <Member>194명의 회원</Member>
           <Writing>
             <Mini>
-              <MiniTitle>C컬 롱라인</MiniTitle>
+              <MiniTitle onClick={() => navigate("/community/feed")}>C컬 롱라인</MiniTitle>
               <MiniMember>위필</MiniMember>
             </Mini>
             <Mini>
-              <MiniTitle>골반 세팅법</MiniTitle>
+              <MiniTitle onClick={() => navigate("/community/feed")}>골반 세팅법</MiniTitle>
               <MiniMember>클래식</MiniMember>
             </Mini>
             <Mini>
-              <MiniTitle>힐링필라테스</MiniTitle>
+              <MiniTitle onClick={() => navigate("/community/feed")}>힐링필라테스</MiniTitle>
               <MiniMember>힐러</MiniMember>
             </Mini>
           </Writing>
