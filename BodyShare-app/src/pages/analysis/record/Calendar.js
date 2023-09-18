8f84dd09ce1b4635b4c916e4242200f4ce994f0d
LAPTOP-NTEF1TGQ\tlstj <tlstj212@naver.com>
2023-09-19 02:37:28 +0900

    refactor: 컴포넌트 분리 및 코드 수정(이전 날짜반영 오류 수정)

--- BodyShare-app/src/pages/analysis/record/Calendar.js
+++ BodyShare-app/src/pages/analysis/record/Calendar.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 import React, { useState } from 'react';
-import {useNavigate } from "react-router-dom";
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import buttonplus from "../../../assets/Img/buttonplus.png";
-import seemore from "../../../assets/Img/seemore.png";
+import Add from "pages/analysis/record/NoteAdd"
+import SportRecord from "pages/analysis/record/SportNote"
+import FoodRecord from "pages/analysis/record/FoodNote"
 
 
 const RecordGrid = styled.div`
@@ -42,111 +42,6 @@ const P = styled.p`
   margin: 0;
 `;
 
-const SportNoteContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr auto auto;
-  align-items: center;
-  padding: 20px;
-`;
-
-const FoodNoteContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr auto auto;
-  align-items: center;
-  padding: 20px;
-`;
-
-const SportNote = styled.p`
-  font-size: 14px;
-`;
-
-const SportTime = styled.p`
-  font-size: 14px;
-`;
-
-const SeeMore = styled.button`
-  width: 20px;
-  height: 26px;
-  border: none;
-  background-color: white;
-  cursor: pointer;
-  padding: 0;
-`;
-
-const FoodSeeMore = styled.button`
-  width: 20px;
-  height: 26px;
-  border: none;
-  background-color: white;
-  cursor: pointer;
-  padding: 0;
-`;
-
-const SeeMoreDetail = styled.div`
-  width: 158px;
-  position: absolute; 
-  top: 70px;
-  left: 200px;
-  display: ${({ isVisible }) => (isVisible ? 'block' : 'none')};
-`;
-
-const FoodSeeMoreDetail = styled.div`
-  width: 158px;
-  position: absolute; 
-  top: 130px;
-  left: 200px;
-  display: ${({ isVisible }) => (isVisible ? 'block' : 'none')};
-`;
-// const Correction = styled.button`
-//   width: 158px;
-//   height: 40px;
-//   border-radius: 15px 15px 0px 0px;
-//   border: 1px solid #B3B3B3;
-//   background-color: white;
-//   &:hover {
-//     cursor: pointer;
-//     background-color: #556FFF;
-//   }
-// `;
-
-const Delete = styled.button`
-  width: 158px;
-  height: 40px;
-  border-radius: 15px;
-  border: 1px solid #B3B3B3;
-  background-color: white;
-
-  &:hover {
-    cursor: pointer;
-    background-color: #556FFF;
-  }
-`;
-
-const FoodDelete = styled.button`
-  width: 158px;
-  height: 40px;
-  border-radius: 15px;
-  border: 1px solid #B3B3B3;
-  background-color: white;
-  &:hover {
-    cursor: pointer;
-    background-color: #556FFF;
-  }
-`;
-
-const SmIng = styled.img`
-  width: 20px;
-  height: 26px;
-`;
-
-const FoodNote = styled.p`
-  font-size: 14px;
-`;
-
-const NoteAddGrid = styled.div`
-  display: grid;
-  gap: 10px;
-`;
 
 const Line = styled.div`
   width: 390px;
@@ -154,56 +49,26 @@ const Line = styled.div`
   background-color: rgba(135, 135, 135, 0.3);
 `;
 
-const Add = styled.div`
-  text-align: left;
-  padding: 10px 20px;
-  border-radius: 15px;
-  background-color: rgba(85, 111, 255, 0.3);
-  display: flex;
-`;
 
-const BtImg = styled.img`
-  width: 19px;
-  border: none;
-  background: none;
-  margin-right: 10px;
-
-  &:hover {
-    cursor: pointer;
-  }
-`;
 
 const Record = function () {
-  const navigate = useNavigate();
   // 캘린더
   const [value, onChange] = useState(new Date());
   const [selectedDate, setSelectedDate] = useState(new Date());
   
-  const [seeMoreVisible, setSeeMoreVisible] = useState(false);
-  const [foodSeeMoreVisible, setFoodSeeMoreVisible] = useState(false);
-
   const handleDateChange = (date) => {
     // onChange();
     // setSelectedDate(date);
 
     const today = new Date();
     today.setHours(0,0,0,0);
+    setSelectedDate(date);
 
     if (date >= today){
       onChange(date);
-      setSelectedDate(date);
     }
   };
 
-  const toggleSeeMore = () => {
-    setFoodSeeMoreVisible(false);
-    setSeeMoreVisible(!seeMoreVisible);
-  };
-
-  const toggleFoodSeeMore = () => { 
-    setSeeMoreVisible(false);
-    setFoodSeeMoreVisible(!foodSeeMoreVisible);
-  };
 
   return (
     <>
@@ -213,45 +78,25 @@ const Record = function () {
             onChange={handleDateChange} 
             value={value}
             // 현재 날짜를 최대로 잡음
-            maxDate={new Date()} 
+            maxDate={new Date()}
           />
         </CalendarDiv>
+
         <NoteGrid>
+          {/* 날짜 부분 */}
           <TitleDate>
             <P>{selectedDate ? selectedDate.toLocaleDateString() : ""}</P>
           </TitleDate>
-          <SportNoteContainer>
-            <SportNote>유산소 달리기</SportNote>
-            <SportTime>30분</SportTime>
-            <SeeMore onClick={toggleSeeMore}>
-              <SmIng src={seemore}></SmIng>
-            </SeeMore>
-            <SeeMoreDetail isVisible={seeMoreVisible}>
-              {/* <Correction onClick={() => navigate("/analysis/time")}>수정하기</Correction> */}
-              <Delete>삭제하기</Delete>
-            </SeeMoreDetail>
-          </SportNoteContainer>
+          {/* 운동 기록 부분 */}
+          <SportRecord />
           <Line></Line>
-          <FoodNoteContainer>
-            <FoodNote>닭죽</FoodNote>
-            <FoodSeeMore onClick={toggleFoodSeeMore}>
-              <SmIng src={seemore}></SmIng>
-            </FoodSeeMore>
-            <FoodSeeMoreDetail isVisible={foodSeeMoreVisible}>
-              <FoodDelete>삭제하기</FoodDelete>
-            </FoodSeeMoreDetail>
-          </FoodNoteContainer>
+          {/* 식단 기록 부분 */}
+          <FoodRecord />
         </NoteGrid>
-        <NoteAddGrid>
-          <Add>
-            <BtImg src={buttonplus} onClick={() => navigate("/analysis/add")}></BtImg>
-            <P>운동 기록 추가</P>
-          </Add>
-          <Add>
-            <BtImg src={buttonplus} onClick={() => navigate("/analysis/add/food")}></BtImg>
-            <P>식단 기록 추가</P>
-          </Add>
-        </NoteAddGrid>
+
+        {/* 기록 추가하기 */}
+        <Add />
+
       </RecordGrid>
     </>
   );
