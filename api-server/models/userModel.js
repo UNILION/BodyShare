fb9c50c65a1d781d8892ce28f4bb03c3b9f809ca
Dongyun <kj77181@gmail.com>
2023-09-18 17:23:48 +0900

    feat: 관심사 운동 식단 라우터 등록

--- api-server/models/userModel.js
+++ api-server/models/userModel.js
@@ -43,8 +43,40 @@ const userModel = {
     } catch (err) {
       throw new Error("DB Error", { cause: err });
     }
-  }
+  },
 
+  //회원 관심사 등록
+  async createInterest(user) {
+    try {
+      const sql = `insert into userInterest set ?`;
+      const [result] = await pool.query(sql, [user]);
+      return result.insertId;
+    } catch (err) {
+      throw new Error("DB Error", { cause: err });
+    }
+  },
+
+  //회원 관심사 수정
+  async updateInterest(no, user) {
+    try {
+      const sql = `update userInterest set ? where userNo = ?`;
+      const [result] = await pool.query(sql, [user, no]);
+      return result.affectedRows;
+    } catch (err) {
+      throw new Error("DB Error", { cause: err });
+    }
+  },
+
+  // 회원 관심사 조회
+  async findByNoInterest(no) {
+    try {
+      const sql = `select * from userInterest where userNo = ?`;
+      const [result] = await pool.query(sql, [no]);
+      return result;
+    } catch (err) {
+      throw new Error("DB Error", { cause: err });
+    }
+  }
 };
 
 module.exports = userModel;
