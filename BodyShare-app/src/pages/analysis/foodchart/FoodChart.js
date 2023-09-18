56b84b3f36851118ed5242c237b958828914d824
LAPTOP-NTEF1TGQ\tlstj <tlstj212@naver.com>
2023-09-18 11:01:42 +0900

    feat:슬라이드 적용

--- BodyShare-app/src/pages/analysis/foodchart/FoodChart.js
+++ BodyShare-app/src/pages/analysis/foodchart/FoodChart.js
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { Chart } from 'react-google-charts';
+import React from "react";
+import styled from "styled-components";
+import { Chart } from "react-google-charts";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const SliderContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 100%); 
+  grid-template-columns: repeat(2, 100%);
   overflow: hidden;
-  width: 360px;
+  width: 370px;
   margin: 35px auto;
+
+.slick-dots {
+  position: absolute;
+  bottom: 10px;
+}
+
 `;
 
 const Slide = styled.div`
@@ -15,35 +24,11 @@ const Slide = styled.div`
   display: grid;
   place-items: center;
   transition: transform 0.3s ease;
-  grid-column: span 1; 
+  grid-column: span 1;
 `;
 
-const Slider = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 100%);
-  transition: transform 0.3s ease;
-  transform: translateX(-${props => props.slideIndex * 100}%);
-`;
-
-const ButtonContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, auto);
-  justify-content: center;
-`;
-
-const Button = styled.button`
-  background-color: ${({ isBlack }) => (isBlack ? 'black' : '#878787')};
-  color: #fff;
-  border: none;
-  border-radius: 50%;
-  padding: 5px 5px;
-  margin: 10px;
-  cursor: pointer;
-`;
-
-// 차트 스타일
 const ChartBox = styled.div`
-  width: 350px;
+  width: 360px;
   height: 476px;
   border-radius: 30px;
   background-color: #ffffff;
@@ -51,123 +36,100 @@ const ChartBox = styled.div`
   margin: 0 auto;
   box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
   cursor: pointer;
-`;
-
-const ChartContainer1 = styled.div`
-  width: 350px;
-  height: 476px;
-  background-color: white;
-  border-radius: 15px;
-  border: 1px solid rgba(135, 135, 135, 0.3);
-  cursor: pointer;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-const ChartContainer2 = styled.div`
-  width: 350px;
+const ChartContainer = styled.div`
+  width: 340px;
   height: 476px;
   background-color: white;
   border-radius: 15px;
-  border: 1px solid rgba(135, 135, 135, 0.3);
+  /* border: 1px solid rgba(135, 135, 135, 0.3); */
   cursor: pointer;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  margin-left: 10px;
 `;
 
-const FoodChart = function() {
-  const [slideIndex, setSlideIndex] = useState(0);
-
-  const nextSlide = () => {
-    setSlideIndex(slideIndex + 1);
-  };
-
-  const prevSlide = () => {
-    setSlideIndex(slideIndex - 1);
-  };
-
+const FoodChart = function () {
   // 차트
   const chartData1 = [
     ["작업", "하루 시간"],
     ["탄", 3],
     ["단", 5],
-    ["지", 2]
+    ["지", 2],
   ];
 
   const chartOptions1 = {
     title: "Calorie",
     pieHole: 0.4,
     titleTextStyle: {
-      fontSize: 18,
-    }
+      fontSize: 20,
+    },
+    chartArea: {
+      left: "10%", 
+      top: "20%", 
+      width: "100%",
+      height: "100%",
+    },
   };
 
   const chartData2 = [
-    ['요일', '칼로리'],
-    ['월',  1000],
-    ['화',  1170],
-    ['수',  660],
-    ['목',  1030],
-    ['금',  1030],
-    ['토',  1030],
-    ['일',  1030]
+    ["요일", "칼로리"],
+    ["월", 1000],
+    ["화", 1170],
+    ["수", 660],
+    ["목", 1030],
+    ["금", 1030],
+    ["토", 1030],
+    ["일", 1030],
   ];
 
   const chartOptions2 = {
-    title: '요일별 칼로리 섭취량',
-    curveType: 'function'
+    title: "요일별 칼로리 섭취량",
+    curveType: "function",
+  };
+
+  // 슬라이드
+  const settings = {
+    dots: true,
+    infinite: false
   };
 
   return (
-    <>
-      <SliderContainer>
-        <Slider slideIndex={slideIndex}>
+    <SliderContainer>
+      <Slider {...settings}>
         <Slide>
           <ChartBox>
-            <ChartContainer1>
-                <Chart
-                  chartType="PieChart"
-                  width="350px"
-                  height="476px"
-                  data={chartData1}
-                  options={chartOptions1}
-                  graph_id="donutchart"
-                />
-              </ChartContainer1>
+            <ChartContainer>
+              <Chart
+                chartType="PieChart"
+                width="100%"
+                height="456px"
+                data={chartData1}
+                options={chartOptions1}
+                graph_id="donutchart"
+              />
+            </ChartContainer>
           </ChartBox>
-          </Slide>
-          <Slide>
-          <ChartBox>
-            <ChartContainer2>
-                <Chart
-                  chartType="ComboChart"
-                  width="340px"
-                  height="476px"
-                  data={chartData2}
-                  options={chartOptions2}
-                  graph_id="curve_chart"
-                />
-              </ChartContainer2>
-          </ChartBox>
-          </Slide>
-        </Slider>
-      </SliderContainer>
+        </Slide>
 
-      <ButtonContainer>
-        <Button
-          onClick={prevSlide}
-          disabled={slideIndex === 0}
-          isBlack={slideIndex === 0}
-        ></Button>
-        <Button
-          onClick={nextSlide}
-          disabled={slideIndex === 1}
-          isBlack={slideIndex === 1}
-        ></Button>
-      </ButtonContainer>
-    </>
+        <Slide>
+          <ChartBox>
+            <ChartContainer>
+              <Chart
+                chartType="ComboChart"
+                width="340px"
+                height="456px"
+                data={chartData2}
+                options={chartOptions2}
+                graph_id="curve_chart"
+              />
+            </ChartContainer>
+          </ChartBox>
+        </Slide>
+      </Slider>
+    </SliderContainer>
   );
 };
 
--- BodyShare-app/src/pages/analysis/sportchart/SportChart.js
+++ BodyShare-app/src/pages/analysis/sportchart/SportChart.js
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { Chart } from 'react-google-charts';
+import React from "react";
+import styled from "styled-components";
+import { Chart } from "react-google-charts";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const SliderContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 100%);
   overflow: hidden;
-  width: 360px;
+  width: 370px;
   margin: 35px auto;
+
+.slick-dots {
+  position: absolute;
+  bottom: 10px;
+}
+
 `;
 
 const Slide = styled.div`
@@ -16,32 +27,8 @@ const Slide = styled.div`
   grid-column: span 1;
 `;
 
-const Slider = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 100%);
-  transition: transform 0.3s ease;
-  transform: translateX(-${(props) => props.slideIndex * 100}%);
-`;
-
-const ButtonContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, auto);
-  justify-content: center;
-`;
-
-const Button = styled.button`
-  background-color: ${({ isBlack }) => (isBlack ? 'black' : '#878787')};
-  color: #fff;
-  border: none;
-  border-radius: 50%;
-  padding: 5px 5px;
-  margin: 10px;
-  cursor: pointer;
-`;
-
-// 차트 스타일
-const ChartBox1 = styled.div`
-  width: 350px;
+const ChartBox = styled.div`
+  width: 360px;
   height: 476px;
   border-radius: 30px;
   background-color: #ffffff;
@@ -54,138 +41,96 @@ const ChartBox1 = styled.div`
   align-items: center;
 `;
 
-const ChartBox2 = styled.div`
-  width: 350px;
-  height: 476px;
-  border-radius: 30px;
-  background-color: #ffffff;
-  border: 1px solid rgba(0, 0, 0, 0.25);
-  margin: 0 auto;
-  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ChartContainer1 = styled.div`
-  width: 350px;
+const ChartContainer = styled.div`
+  width: 340px;
   height: 476px;
   background-color: white;
   border-radius: 15px;
-  border: 1px solid rgba(135, 135, 135, 0.3);
+  /* border: 1px solid rgba(135, 135, 135, 0.3); */
   cursor: pointer;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ChartContainer2 = styled.div`
-  width: 350px;
-  height: 476px;
-  background-color: white;
-  border-radius: 15px;
-  border: 1px solid rgba(135, 135, 135, 0.3);
-  cursor: pointer;
+  margin-left: 10px;
 `;
 
 const SportChart = function () {
-  const [slideIndex, setSlideIndex] = useState(0);
+// 차트 데이터
+const chartData1 = [
+  ['', '운동 분'],
+  ['월', 60],
+  ['화', 45],
+  ['수', 30],
+  ['목', 75],
+  ['금', 90],
+  ['토', 120],
+  ['일', 60],
+];
 
-  const nextSlide = () => {
-    setSlideIndex(slideIndex + 1);
-  };
+const chartOptions1 = {
+  legend: { position: 'none' },
+  chart: {
+    title: 'Daily Diet',
+  },
+};
 
-  const prevSlide = () => {
-    setSlideIndex(slideIndex - 1);
-  };
+const chartData2 = [
+  ["Task", "운동칼로리", "섭취칼로리"],
+  ["월", 8, 2],
+  ["화", 2, 1],
+  ["수", 4, 3],
+  ["목", 10, 5],
+  ["금", 10, 6],
+  ["토", 10, 4],
+  ["일", 10, 7]
+];
 
-  // 차트 데이터
-  const chartData1 = [
-    ['', '운동 분'],
-    ['월', 60],
-    ['화', 45],
-    ['수', 30],
-    ['목', 75],
-    ['금', 90],
-    ['토', 120],
-    ['일', 60],
-  ];
+const chartOptions2 = {
+  title: "칼로리 계산(운동으로 소비한 칼로리 - 섭취한 칼로리)",
+  legend: { position: "top" },
+  hAxis: { title: "칼로리" },
+  vAxis: { title: "요일" }, 
+  bars: "horizontal", 
+};
 
-  const chartOptions1 = {
-    legend: { position: 'none' },
-    chart: {
-      title: 'Daily Diet',
-    },
-  };
 
-  const chartData2 = [
-    ["Task", "운동칼로리", "섭취칼로리"],
-    ["월", 8, 2],
-    ["화", 2, 1],
-    ["수", 4, 3],
-    ["목", 10, 5],
-    ["금", 10, 6],
-    ["토", 10, 4],
-    ["일", 10, 7]
-  ];
-  
-  const chartOptions2 = {
-    title: "칼로리 계산(운동으로 소비한 칼로리 - 섭취한 칼로리)",
-    legend: { position: "top" },
-    hAxis: { title: "칼로리" },
-    vAxis: { title: "요일" }, 
-    bars: "horizontal", 
+  // 슬라이드
+  const settings = {
+    dots: true,
+    infinite: false
   };
 
   return (
-    <>
-      <SliderContainer>
-        <Slider slideIndex={slideIndex}>
-          <Slide>
-              <ChartBox1>
-                <ChartContainer1>
-                  <Chart
-                    chartType="Bar"
-                    width="300px"
-                    height="460px"
-                    data={chartData1}
-                    options={chartOptions1}
-                    graph_id="barchart1"
-                  />
-                </ChartContainer1>
-              </ChartBox1>
-          </Slide>
-          <Slide>
-              <ChartBox2>
-                <ChartContainer2>
-                <Chart
-                    chartType="BarChart"
-                    width="350px"
-                    height="476px"
-                    data={chartData2}
-                    options={chartOptions2}
-                    graph_id="barchart2"
-                />
-                </ChartContainer2>
-              </ChartBox2>
-          </Slide>
-        </Slider>
-      </SliderContainer>
+    <SliderContainer>
+      <Slider {...settings}>
+        <Slide>
+          <ChartBox>
+            <ChartContainer>
+            <Chart
+              chartType="BarChart" 
+              width="320px"
+              height="436px"
+              data={chartData1}
+              options={{chartOptions1, orientation: 'horizontal'}}
+              graph_id="barchart1"
+            />
+            </ChartContainer>
+          </ChartBox>
+        </Slide>
 
-      <ButtonContainer>
-        <Button
-          onClick={prevSlide}
-          disabled={slideIndex === 0}
-          isBlack={slideIndex === 0}
-        ></Button>
-        <Button
-          onClick={nextSlide}
-          disabled={slideIndex === 1}
-          isBlack={slideIndex === 1}
-        ></Button>
-      </ButtonContainer>
-    </>
+        <Slide>
+          <ChartBox>
+            <ChartContainer>
+            <Chart
+              chartType="BarChart" 
+              width="330px"
+              height="436px"
+              data={chartData2}
+              options={chartOptions2}
+              graph_id="barchart2"
+            />
+            </ChartContainer>
+          </ChartBox>
+        </Slide>
+      </Slider>
+    </SliderContainer>
   );
 };
 
