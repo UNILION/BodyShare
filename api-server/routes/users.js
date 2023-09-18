35a0967ec2bfa7ba73b124bbf54fdda7723a4cca
Dongyun <kj77181@gmail.com>
2023-09-18 14:31:18 +0900

    feat: 라우트 설정

--- api-server/routes/users.js
+++ api-server/routes/users.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const user = require("../models/userModel");
+
+// 회원 상세 조회
+router.get("/:no", async (req, res, next) => {
+  try{
+    const no = Number(req.params.no);
+    const result = await user.findById(no);
+    res.json(result);
+  }catch(err){
+    next(err);
+  }
+});
+
+// 회원 가입
+router.post("/signup", async (req, res, next) => {
+  try{
+    const id = await user.create(req.body);
+    res.json({ id });
+  }catch(err){
+    next(err);
+  }
+});
+
+// 회원 정보 수정
+router.put("/:no", async (req, res, next) => {
+  try{
+    const no = Number(req.params.no);
+    const count = await user.update(no, req.body);
+    res.json({ count });
+  }catch(err){
+    next(err);
+  }
+});
+
+// 로그인
+router.post("/signin", async (req, res, next) => {
+  try{
+    const ok = await user.signin(req.body);
+    res.json({ ok });
+  }catch(err){
+    next(err);
+  }
 });
 
 module.exports = router;
