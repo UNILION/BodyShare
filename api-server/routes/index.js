6d08ee09d20cd2ef74223b48e71cebccd06e47d3
Dongyun <kj77181@gmail.com>
2023-09-19 11:43:33 +0900

    feat: 게시물 및 댓글 라우터 추가

--- api-server/routes/index.js
+++ api-server/routes/index.js
@@ -6,12 +6,16 @@ const commuRouter = require("./community");
 const sportsRouter = require("./sports");
 const foodRouter = require("./food");
 const recordRouter = require("./record");
+const postRouter = require("./post");
+const commentRouter = require("./comment");
 
 router.use("/users", userRouter);
 router.use("/community", commuRouter);
 router.use("/sports", sportsRouter);
 router.use("/food", foodRouter);
 router.use("/record", recordRouter);
+router.use("/post", postRouter);
+router.use("/comment", commentRouter);
 
 
 module.exports = router;
