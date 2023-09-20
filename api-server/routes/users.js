f1a1193d604487243897533d3ae03d34cefc9939
Dongyun <kj77181@gmail.com>
2023-09-20 13:24:39 +0900

    feat: 유저 프로필 및 배너 사진 관련 multer 기능 추가

--- api-server/routes/users.js
+++ api-server/routes/users.js
@@ -2,6 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 const user = require("../models/userModel");
+const multer = require("multer");
+
+const upload = multer({
+  storage: multer.diskStorage({ // 저장한공간 정보 : 하드디스크에 저장
+      destination(req, file, done) { // 저장 위치
+          done(null, 'imgaes/users/'); // uploads라는 폴더 안에 저장
+      },
+      filename(req, file, done) { // 파일명을 어떤 이름으로 올릴지
+          const userNo = req.body.userNo;
+          const fieldname = file.fieldname;
+          const ext = path.extname(file.originalname); // 파일의 확장자
+          if(fieldname == "profileImg"){
+            done(null, `${userNo}_p_${ext}`); // 유저 번호 + 프로필or배너 + 확장자 이름으로 저장
+          }
+          else if(fieldname == "bannerImg"){
+            done(null, `${userNo}_b_${ext}`); // 유저 번호 + 프로필or배너 + 확장자 이름으로 저장
+          }
+      }
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 } // 5메가로 용량 제한
+});
 
 // 회원 상세 조회
 router.get("/user/:no", async (req, res, next) => {
@@ -15,8 +36,12 @@ router.get("/user/:no", async (req, res, next) => {
 });
 
 // 회원 가입
-router.post("/signup", async (req, res, next) => {
+router.post("/signup", upload.single("profileImg") ,async (req, res, next) => {
   try{
+    if(req.file){
+      req.body.profileImageUrl = req.file.filename;
+    }
+    
     const id = await user.create(req.body);
     res.json({ id });
   }catch(err){
@@ -25,9 +50,22 @@ router.post("/signup", async (req, res, next) => {
 });
 
 // 회원 정보 수정
-router.put("/useredit/:no", async (req, res, next) => {
+router.put("/useredit/:no", upload.fields([{name:"profileImg"}, {name:"bannerImg"}]), async (req, res, next) => {
   try{
     const no = Number(req.params.no);
+    // 프로필 이미지와 배너 이미지 가져오기
+    const profileImages = req.files["profileImg"];
+    const bannerImages = req.files["bannerImg"];
+
+    // 파일이 업로드되었을 때만 처리
+    if (profileImages) {
+      req.body.profileImageUrl = profileImages[0].filename;
+    }
+
+    if (bannerImages) {
+      req.body.bannerImageUrl = bannerImages[0].filename;
+    }
+
     const count = await user.update(no, req.body);
     res.json({ count });
   }catch(err){
