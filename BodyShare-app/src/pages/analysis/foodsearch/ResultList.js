1ad2fd440fd0d343e6c62c4e8bd70b035d21380a
LAPTOP-NTEF1TGQ\tlstj <tlstj212@naver.com>
2023-09-21 17:26:06 +0900

    feat:recoil적용 및 aixos 적용 테스트

--- BodyShare-app/src/pages/analysis/foodsearch/ResultList.js
+++ BodyShare-app/src/pages/analysis/foodsearch/ResultList.js
@@ -1,5 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
 import styled from "styled-components";
+import { useRPState } from 'recoil/foodList'; // RP Recoil 상태 불러오기
+import axios from 'axios'; 
+
+const instance = axios.create({
+  baseURL: "http://localhost:33000/api",
+  withCredentials: true
+});
 
 const ResultList = styled.div`
   margin-top: 20px;
@@ -32,7 +39,10 @@ const Line = styled.div`
   border: 1px solid rgba(135, 135, 135, 0.3);
 `;
 
-const ResultCate = function () {
+const ResultCate = function ({ no, name }) {
+
+  // RP 상태를 읽고 업데이트할 Recoil 사용
+  const [rp, setRP] = useRPState();
   
   const [buttonStates, setButtonStates] = useState({
     button1: false,
@@ -42,33 +52,55 @@ const ResultCate = function () {
     button5: false,
   });
 
-  const handleButtonClick = (buttonName) => {
+  useEffect(() => {
+    // 서버에서 음식 데이터를 가져오는 요청
+    const fetchFoodData = async () => {
+      try {
+        const response = await instance.get(`/add/food?id=${no}`);
+        const data = response.data;
+        // 응답 데이터를 적절히 처리하고 필요한 상태 업데이트를 수행
+        console.log("성공", data);
+      } catch (error) {
+        // 요청이 실패하면 에러 처리
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchFoodData();
+  }, [no]);
+
+  const handleButtonClick = (buttonName, rpValue) => {
     setButtonStates((prevState) => ({
       ...prevState,
       [buttonName]: !prevState[buttonName],
     }));
+
+    // RP 값을 업데이트합니다.
+    setRP(rpValue);
   };
+
+
 
   return(
     <>
       <ResultList>
-        <ResultButton 
-          active={buttonStates.button1} 
-          onClick={() => handleButtonClick('button1')}
+      <ResultButton
+          active={buttonStates.button1}
+          onClick={() => handleButtonClick('button1', '닭가슴살')}
           hoverColor="rgba(85, 111, 255, 0.7)"
         >
-            <RP>김치</RP>
-            <Line></Line>
-        </ResultButton>
-        <ResultButton
-          active={buttonStates.button2}
-          onClick={() => handleButtonClick('button2')}
-          hoverColor="rgba(85, 111, 255, 0.7)"
-        >
-          <RP>닭가슴살</RP>
+          {rp === '닭가슴살' && <RP>닭가슴살</RP>}
           <Line></Line>
         </ResultButton>
-        <ResultButton
+        {/* <ResultButton
+          active={buttonStates.button2}
+          onClick={() => handleButtonClick('button2', '닭가슴살')}
+          hoverColor="rgba(85, 111, 255, 0.7)"
+        >
+          {rp === '닭가슴살' && <RP>닭가슴살</RP>}
+          <Line></Line>
+        </ResultButton> */}
+        {/* <ResultButton
           active={buttonStates.button3}
           onClick={() => handleButtonClick('button3')}
           hoverColor="rgba(85, 111, 255, 0.7)"
@@ -91,7 +123,7 @@ const ResultCate = function () {
         >
           <RP>김밥</RP>
           <Line></Line>
-        </ResultButton>
+        </ResultButton> */}
       </ResultList>
     </>
   )
