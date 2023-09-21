5797d9c0d02761957ce94dc931f3b00293f4aa28
JunBeomJang <csi9509@kpu.ac.kr>
2023-09-21 15:02:24 +0900

    feat: 오류 메세지 공통 컴포넌트로 분리

---
+++ BodyShare-app/src/components/commons/Check.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const CheckContainer = styled.div`
+  color:  ${(props) => (props.ccolor ? props.ccolor : "red")};
+  font-size:  ${(props) => (props.cfs ? props.cfs : "12px")};
+  margin-top:  ${(props) => (props.cmt ? props.cmt : "5px")};
+  margin-bottom:  ${(props) => (props.cmb ? props.cmb : "10px")};
+`
+
+const Check = function(props) {
+  return(
+    <CheckContainer {...props} />
+  )
+}
+
+export default Check;
