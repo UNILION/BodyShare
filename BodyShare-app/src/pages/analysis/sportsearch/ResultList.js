55154b17d9cc8aa62ef3035edec8f0e7b9bdf92b
LAPTOP-NTEF1TGQ\tlstj <tlstj212@naver.com>
2023-09-21 17:29:16 +0900

    feat: recoil적용

--- BodyShare-app/src/pages/analysis/sportsearch/ResultList.js
+++ BodyShare-app/src/pages/analysis/sportsearch/ResultList.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import styled from "styled-components";
+import { useSPORTState } from 'recoil/sportList'; // RP Recoil 상태 불러오기
+import axios from 'axios'; // Axios를 불러옵니다.
 
 const ResultList = styled.div`
   margin-top: 20px;
@@ -32,7 +34,9 @@ const Line = styled.div`
   border: 1px solid rgba(135, 135, 135, 0.3);
 `;
 
-const ResultCate = function () {
+const ResultCate = function ({no, name}) {
+
+  const [rp, setRP] = useSPORTState();
   
   const [buttonStates, setButtonStates] = useState({
     button1: false,
@@ -41,51 +45,54 @@ const ResultCate = function () {
     button4: false
   });
 
-  const handleButtonClick = (buttonName) => {
+  const handleButtonClick = (buttonName, rpValue) => {
     setButtonStates((prevState) => ({
       ...prevState,
       [buttonName]: !prevState[buttonName],
     }));
+
+    // RP 값을 업데이트합니다.
+    setRP(rpValue);
   };
 
   return(
     <>
-     <ResultList>
-        <ResultButton 
-        active={buttonStates.button1} 
-        onClick={() => handleButtonClick('button1')}
-        hoverColor="rgba(85, 111, 255, 0.7)"
-        >
-          <RP>축구</RP>
-          <Line></Line>
-        </ResultButton>
+      <ResultList>
         <ResultButton
-          active={buttonStates.button2}
-          onClick={() => handleButtonClick('button2')}
+          active={buttonStates.button1}
+          onClick={() => handleButtonClick('button1', '축구')}
           hoverColor="rgba(85, 111, 255, 0.7)"
         >
-          <RP>수영</RP>
+          {rp === '축구' && <RP>축구</RP>}
+          <Line></Line>
+        </ResultButton>
+        {/* <ResultButton
+          active={buttonStates.button2}
+          onClick={() => handleButtonClick('button2', name)}
+          hoverColor="rgba(85, 111, 255, 0.7)"
+        >
+          {rp === name && <RP>{name}</RP>}
           <Line></Line>
         </ResultButton>
         <ResultButton
           active={buttonStates.button3}
-          onClick={() => handleButtonClick('button3')}
+          onClick={() => handleButtonClick('button3', name)}
           hoverColor="rgba(85, 111, 255, 0.7)"
         >
-          <RP>달리기</RP>
+          {rp === name && <RP>{name}</RP>}
           <Line></Line>
         </ResultButton>
         <ResultButton
           active={buttonStates.button4}
-          onClick={() => handleButtonClick('button4')}
+          onClick={() => handleButtonClick('button4', name)}
           hoverColor="rgba(85, 111, 255, 0.7)"
         >
-          <RP>필라테스</RP>
+          {rp === name && <RP>{name}</RP>}
           <Line></Line>
-        </ResultButton>
+        </ResultButton> */}
       </ResultList>
     </>
   )
 };
 
-export default ResultCate
+export default ResultCate;
