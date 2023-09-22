d2413637b91c45dbca3983637acae0fc41268c00
Dongyun <kj77181@gmail.com>
2023-09-22 18:12:52 +0900

    refactor: 로컬 DB 주소로 변경

--- api-server/config/index.js
+++ api-server/config/index.js
@@ -1,9 +1,19 @@
+// module.exports = {
+//   mysql: {
+//     host: "43.202.150.252",
+//     port: "3306",
+//     database: "bodyshare",
+//     user: "bodyshare",
+//     password: "Bodyshare12$$"
+//   }
+// };
+
 module.exports = {
   mysql: {
-    host: "43.202.150.252",
+    host: "127.0.0.1",
     port: "3306",
     database: "bodyshare",
-    user: "bodyshare",
-    password: "Bodyshare12$$"
+    user: "root",
+    password: "1234"
   }
 };
