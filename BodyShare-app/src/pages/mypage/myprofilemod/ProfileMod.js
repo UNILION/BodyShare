6fd4ebf1b2acf1b5ed33e48d1c414d8e2044bab2
ahn <nameisb22@naver.com>
2023-09-22 18:00:27 +0900

    feat: useform 유효성 검사

--- BodyShare-app/src/pages/mypage/myprofilemod/ProfileMod.js
+++ BodyShare-app/src/pages/mypage/myprofilemod/ProfileMod.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import React, { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
 
 const Titleo = styled.p`
   margin-top: 20px;
@@ -76,10 +77,14 @@ const Ul = styled.ul`
 `
 
 const Warn = styled.p`
-    margin-top: 7px;
+  margin-top: 7px;
   font-size: 11px;
   color: red;
   margin-left: 100px;
+  display: grid;
+  grid-template-columns: auto;
+  align-items: center;
+  width: 260px;
 `
 
 const BodyDiv = styled.div`
@@ -124,6 +129,19 @@ const BodyP = styled.div`
 
 
 const ProfileMod = function ({ password, nickname, height, weight }) {
+  const {
+    register,
+    control,
+    handleSubmit,
+    getValues,
+    formState: { errors }
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  const validatePassword = (value, password) => {
+    return value === password || "기존 비밀번호와 다릅니다.";
+  };
+
   const [heightInput, setHeightInput] = useState(height);
   const [weightInput, setWeightInput] = useState(weight);
 
@@ -151,63 +169,148 @@ const ProfileMod = function ({ password, nickname, height, weight }) {
     setWeightInput(formattedValue);
   };
 
+  const hidePassword = (password) => {
+    // 비밀번호 문자열의 첫 번째 문자를 제외한 나머지를 '*'로 대체
+    return password.charAt(0) + '*'.repeat(password.length - 1);
+  };
+
 
   return (
     <>
-      <Titleo>배너사진</Titleo>
-      <BannerPic>
-        <PicInput type="file"
-          accept="image/gif, image/jpeg, image/png, image/jpg" name="bannerImg">
-        </PicInput>
-      </BannerPic>
-      <TitleT>프로필 사진</TitleT>
-      <ProfilePic>
-        <PicInput type="file"
-          accept="image/gif, image/jpeg, image/png, image/jpg" name="profileImg">
-        </PicInput>
-      </ProfilePic>
-      <Ul>
-        <Titlel>닉네임</Titlel>
-        <Input placeholder={nickname}></Input>
-      </Ul>
-      <Warn>중복된 닉네임입니다.</Warn>
-      <Ul>
-        <Titlel>기존 비밀번호</Titlel>
-        <Input type="password" placeholder={password}></Input>
-      </Ul>
-      <Warn>기존 비밀번호와 다릅니다.</Warn>
-      <Ul>
-        <Titlel>변경 비밀번호</Titlel>
-        <Input type="password" placeholder="변경 비밀번호"></Input>
-      </Ul>
-      <Warn>조건에 충족하지 않습니다.</Warn>
-      <Ul>
-        <Titlel>비밀번호 확인</Titlel>
-        <Input type="password" placeholder="변경 비밀번호"></Input>
-      </Ul>
-      <Warn>비밀번호가 다릅니다.</Warn>
-      <BodyDiv>
-        <Bodydetail>
-          <Titlej>키</Titlej>
-          <BInput
-            placeholder={height}
-            value={heightInput}
-            onChange={handleHeightChange} // 값이 변경될 때 실행될 함수 설정
-            type="text" // 숫자만 입력되도록 설정
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Titleo>배너사진</Titleo>
+        <BannerPic>
+          <PicInput type="file"
+            accept="image/gif, image/jpeg, image/png, image/jpg" name="bannerImg">
+          </PicInput>
+        </BannerPic>
+        <TitleT>프로필 사진</TitleT>
+        <ProfilePic>
+          <PicInput type="file"
+            accept="image/gif, image/jpeg, image/png, image/jpg" name="profileImg">
+          </PicInput>
+        </ProfilePic>
+        <Ul>
+          <Titlel>닉네임</Titlel>
+          <Input placeholder={nickname}></Input>
+        </Ul>
+        <Warn>중복된 닉네임입니다.</Warn>
+        <Ul>
+          <Titlel>기존 비밀번호</Titlel>
+          <Controller
+            name="currentPassword"
+            control={control}
+            rules={{
+              required: "기존 비밀번호를 입력하세요.",
+              validate: (value) => validatePassword(value, password),
+            }}
+            render={({ field }) => (
+              <>
+                <Input
+                  {...field}
+                  type="password"
+                  id="currentPassword"
+                  placeholder={hidePassword(password)}
+                />
+                <Warn>
+                  {errors.currentPassword && (
+                    <p style={{ color: "red" }}>{errors.currentPassword.message}</p>
+                  )}
+                </Warn>
+              </>
+            )}
           />
-          <BodyP>cm</BodyP>
-        </Bodydetail>
-        <Bodydetail>
-          <Titlej>몸무게</Titlej>
-          <BInput
-            placeholder={weight}
-            value={weightInput}
-            onChange={handleWeightChange} // 값이 변경될 때 실행될 함수 설정
-            type="text" // 숫자만 입력되도록 설정
+        </Ul>
+        <Ul>
+          <Titlel>변경 비밀번호</Titlel>
+          <Controller
+            name="password"
+            control={control}
+            rules={{
+              required: "비밀번호를 입력하세요.",
+              minLength: {
+                value: 8,
+                message: "비밀번호는 최소 8자 이상이어야 합니다.",
+              },
+              pattern: {
+                value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$/,
+                message:
+                  "비밀번호는 대문자, 소문자, 숫자, 특수 문자(@#$%^&+=)를 최소 한 개씩 포함해야 합니다.",
+              },
+            }}
+            render={({ field }) => (
+              <>
+                <Input
+                  {...field}
+                  type="password"
+                  id="password"
+                  placeholder="비밀번호 입력"
+                />
+                <Warn>
+                  {errors.password ? (
+                    <p>{errors.password.message}</p>
+                  ) : (
+                    <p style={{ display: "none" }} />
+                  )}
+                </Warn>
+              </>
+            )}
           />
-          <BodyP>kg</BodyP>
-        </Bodydetail>
-      </BodyDiv>
+        </Ul>
+        <Ul>
+          <Titlel>비밀번호 확인</Titlel>
+          <Controller
+            name="passwordConfirm"
+            control={control}
+            rules={{
+              required: "비밀번호를 다시 입력하세요.",
+              validate: (value) =>
+                value === getValues("password") ||
+                "비밀번호와 일치하지 않습니다.",
+            }}
+            render={({ field }) => (
+              <>
+                <Input
+                  {...field}
+                  type="password"
+                  id="passwordConfirm"
+                  placeholder="비밀번호 확인"
+                />
+                <Warn>
+                  {errors.passwordConfirm ? (
+                    <p>{errors.passwordConfirm.message}</p>
+                  ) : (
+                    <p style={{ display: "none" }} />
+                  )}
+                </Warn>
+              </>
+            )}
+          />
+        </Ul>
+        <BodyDiv>
+          <Bodydetail>
+            <Titlej>키</Titlej>
+            <BInput
+              placeholder={height}
+              value={heightInput}
+              onChange={handleHeightChange} // 값이 변경될 때 실행될 함수 설정
+              type="text" // 숫자만 입력되도록 설정
+            />
+            <BodyP>cm</BodyP>
+          </Bodydetail>
+          <Bodydetail>
+            <Titlej>몸무게</Titlej>
+            <BInput
+              placeholder={weight}
+              value={weightInput}
+              onChange={handleWeightChange} // 값이 변경될 때 실행될 함수 설정
+              type="text" // 숫자만 입력되도록 설정
+            />
+            <BodyP>kg</BodyP>
+          </Bodydetail>
+        </BodyDiv>
+      </form>
+
     </>
   );
 };
