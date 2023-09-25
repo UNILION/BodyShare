f40474cf74dcbca64a7af27f793eb3370bb826bd
Dongyun <kj77181@gmail.com>
2023-09-25 16:38:42 +0900

    refactor: DB 주소 복구

--- api-server/config/index.js
+++ api-server/config/index.js
@@ -1,19 +1,19 @@
-// module.exports = {
-//   mysql: {
-//     host: "43.202.150.252",
-//     port: "3306",
-//     database: "bodyshare",
-//     user: "bodyshare",
-//     password: "Bodyshare12$$"
-//   }
-// };
-
 module.exports = {
   mysql: {
-    host: "192.168.75.156",
+    host: "43.202.150.252",
     port: "3306",
     database: "bodyshare",
-    user: "root",
-    password: "1234"
+    user: "bodyshare",
+    password: "Bodyshare12$$"
   }
 };
+
+// module.exports = {
+//   mysql: {
+//     host: "192.168.75.156",
+//     port: "3306",
+//     database: "bodyshare",
+//     user: "root",
+//     password: "1234"
+//   }
+// };
