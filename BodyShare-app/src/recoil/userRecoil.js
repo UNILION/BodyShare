4d839716883a85806bcf8f34fc8b3da7bcb14b31
Dongyun <kj77181@gmail.com>
2023-09-26 11:28:29 +0900

    feat: 유저 관심사 선택 recoil 추가

--- BodyShare-app/src/recoil/userRecoil.js
+++ BodyShare-app/src/recoil/userRecoil.js
@@ -19,3 +19,20 @@ export const userSelector = selector({
     return userAtomValue; // 연산 결과 반환
   },
 });
+
+// 아톰(atom) 정의
+export const interestAtom = atom({
+  key: 'interest',
+  default: [], // 초기 상태
+  effects_UNSTABLE: [persistAtom]
+});
+
+// 셀렉터(selector) 정의
+export const interestSelector = selector({
+  key: 'interestSelector',
+  get: ({ get }) => {
+    const interestAtomValue = get(interestAtom); // 아톰 값 가져오기
+    // 여기서 원하는 연산 수행
+    return interestAtomValue; // 연산 결과 반환
+  },
+});
