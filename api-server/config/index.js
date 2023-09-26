6eec3cd74d0301dae52bae21b5f6d13ed5e61e89
Dongyun <kj77181@gmail.com>
2023-09-26 18:10:08 +0900

    refactor: DB 서버 변경

--- api-server/config/index.js
+++ api-server/config/index.js
@@ -1,19 +1,9 @@
 module.exports = {
   mysql: {
-    host: "52.78.0.174",
+    host: "13.124.253.233",
     port: "3306",
     database: "bodyshare",
     user: "bodyshare",
     password: "Bodyshare12$$"
   }
 };
-
-// module.exports = {
-//   mysql: {
-//     host: "192.168.75.156",
-//     port: "3306",
-//     database: "bodyshare",
-//     user: "root",
-//     password: "1234"
-//   }
-// };
