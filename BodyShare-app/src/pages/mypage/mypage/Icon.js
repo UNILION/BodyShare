c4fb0babeb71ffaa4b68555dd09048d4e9cfc71d
ahn <nameisb22@naver.com>
2023-09-26 16:53:14 +0900

    feat: Recoil로 DB값 가져오는 것으로 변경

--- BodyShare-app/src/pages/mypage/mypage/Icon.js
+++ BodyShare-app/src/pages/mypage/mypage/Icon.js
@@ -2,9 +2,9 @@ import styled from "styled-components";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { userSelector } from "recoil/userRecoil";
-import { sportsSelector } from 'recoil/sportList';
 import { useRecoilValue } from "recoil";
 import Tag from "components/commons/Tag";
+import { interestSelector } from "recoil/userRecoil";
 
 const instance = axios.create({
   baseURL: "http://localhost:33000/api",
@@ -44,44 +44,15 @@ const Pfcommentul = styled.div`
 
 const Icon = function ({ id, url }) {
   const userNo = useRecoilValue(userSelector);
-  const sports = useRecoilValue(sportsSelector);
-
+  const interest = useRecoilValue(interestSelector);
   const [userInfo, setUserInfo] = useState();
-  const [sportData, setSportData] = useState([]);
-  const [matchingSportNames, setMatchingSportNames] = useState([]);
 
   const loadUser = async function () {
     try {
       const response = await instance.get(`/users/user/${userNo}`);
       const userDataFromApi = response.data;
-      console.log('user Data:', userDataFromApi);
+
       setUserInfo(userDataFromApi);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const loadSport = async function () {
-    try {
-      const response = await instance.get(`/sports`);
-      const sportsDataFromApi = response.data;
-      console.log('sports Data:', sportsDataFromApi);
-      setSportData(sportsDataFromApi);
-
-      // 스포츠 데이터를 가져온 후, 사용자의 관심 스포츠를 필터링하여 일치하는 스포츠를 설정
-      const userInterestData = await instance.get(`/users/interest/${userNo}`);
-      const userInterestNo = userInterestData.data.map(item => item.sportsNo); // 관심 스포츠 번호 추출
-
-      const matchingSportNames = sportsDataFromApi
-        .filter(sport => userInterestNo.includes(sport.no))
-        .map(sport => sport.name); // 매칭되는 스포츠의 이름 추출
-
-      if (matchingSportNames.length === 0) {
-        // 사용자의 관심 스포츠가 없는 경우에 대한 처리
-        setMatchingSportNames(['No matching sports found']); // 또는 다른 기본값 설정
-      } else {
-        setMatchingSportNames(matchingSportNames);
-      }
     } catch (error) {
       console.error(error);
     }
@@ -89,7 +60,6 @@ const Icon = function ({ id, url }) {
 
   useEffect(() => {
     loadUser();
-    loadSport();
   }, []);
 
   return (
@@ -99,14 +69,9 @@ const Icon = function ({ id, url }) {
         <Pfcommentul>
           <Nickli>{id}</Nickli>
           <Inst>
-            {matchingSportNames.map((sport, index) => (
-              <Tag key={index} tagtitle={sport} ml="5px"/>
+            {interest.map((item, index) => (
+              <Tag key={index} tagtitle={item.name} />
             ))}
-            {/* <Tag>
-              {matchingSportNames.map((sport, index) => (
-                <span key={index}>{sport}</span>
-              ))}
-            </Tag> */}
           </Inst>
         </Pfcommentul>
       </Pf>
