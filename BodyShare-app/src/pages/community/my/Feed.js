fe27a6e297de36b25bc7cf1d699b9ac908504775
JunBeomJang <csi9509@kpu.ac.kr>
2023-09-27 18:18:57 +0900

    feat: 커뮤니티 - my - 새글 제목과 닉네임 보여주는 컴포넌트

---
+++ BodyShare-app/src/pages/community/my/Feed.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+const Mini = styled.div`
+  font-size: 13px;
+  margin-top: 10px;
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  font-weight: bold;
+`;
+
+const MiniTitle = styled.span`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis; /* 말줄임 적용 */
+  cursor: pointer;
+`;
+
+const MiniMember = styled.span`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis; /* 말줄임 적용 */
+`;
+
+const Writing = styled.div``;
+
+const Feed = function ({ feed }) {
+  console.log(feed);
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Writing>
+        <Mini>
+          <MiniTitle onClick={() => navigate("/community/feed")}>
+            {feed.title}
+          </MiniTitle>
+          <MiniMember>{feed.nickname}</MiniMember>
+        </Mini>
+      </Writing>
+    </>
+  );
+};
+
+export default Feed;
