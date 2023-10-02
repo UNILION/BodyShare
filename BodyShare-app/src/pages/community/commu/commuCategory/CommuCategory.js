01f28b239c7887a313e788bce82e28393588316c
ahn <nameisb22@naver.com>
2023-10-03 01:51:22 +0900

    feat: 커뮤니티 생성 페이지 DB 정보 받아오는 작업 진행

--- BodyShare-app/src/pages/community/commu/commuCategory/CommuCategory.js
+++ BodyShare-app/src/pages/community/commu/commuCategory/CommuCategory.js
@@ -1,16 +1,40 @@
 import styled from "styled-components";
 import Top from "./Top";
 import Middle from "./Middle";
+import { useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { interestSelector } from "recoil/userRecoil"
+import backButton from "assets/Img/back.png";
+import Search from "./Search";
 
 const Container = styled.div`
-  padding: 10px 20px;
+  display: grid;
+  width: 390px;
+  height: 696px;
+  grid-template-rows: auto auto auto auto auto auto 1fr;
+  gap: 10px;
+`;
+
+const PreviousButton = styled.button`
+  margin-top: 3px;
+  grid-row: 1;
+  width: 20px;
+  height: 20px;
+  background-image: url(${backButton});
+  background-size: cover;
+  border-radius: 20px;
+  border: none;
+  cursor: pointer;
 `;
 
 const CommuSearch = function () {
+  const navigate = useNavigate();
+  // const interestList = useRecoilValue(interestSelector);
+  
   return (
     <Container>
-      <Top />
-      <Middle />
+      <PreviousButton onClick={() => navigate("/community")} />
+      <Search />
     </Container>
   );
 };
