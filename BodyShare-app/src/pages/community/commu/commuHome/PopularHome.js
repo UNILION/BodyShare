d7d6896626d9d7df85b85bb9e76b694c2ae0e2af
DESKTOP-B0R0VN7\상민 <sangmin9772@naver.com>
2023-10-04 06:21:27 +0900

    feat: 커뮤니티 인기추천 기능 및 오류수정

--- BodyShare-app/src/pages/community/commu/commuHome/PopularHome.js
+++ BodyShare-app/src/pages/community/commu/commuHome/PopularHome.js
@@ -1,63 +1,60 @@
 import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
 import Card from "components/commons/Card";
-import Image1 from "assets/Img/card_image1.png";
-import Image2 from "assets/Img/card_image2.png";
-import Image3 from "assets/Img/card_image3.png";
-import Image4 from "assets/Img/card_image4.png";
-import { useState } from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { userSelector } from "recoil/userRecoil";
+import { useRecoilValue } from 'recoil';
+
+const instance = axios.create({
+  baseURL: "http://localhost:33000/api",
+  withCredentials: true
+});
 
 const Group = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   margin-top: 10px;
-  max-height: 500px; /* 스크롤 가능한 최대 높이 설정 */
+  max-height: 500px;
   overflow-y: auto;
 `;
 
 const PopularHome = function () {
   const navigate = useNavigate();
+  const userNo = useRecoilValue(userSelector);
+
+  const [popularCommunityData, setPopularCommunityData] = useState([]);
+
+  const fetchPopularData = async () => {
+    try {
+      const response = await instance.get("/community/bypopular");
+      const community = response.data;
+      console.log(community);
+      setPopularCommunityData(community);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPopularData(); 
+  }, []); 
 
   return (
-    <>
-      <Group>
-        <Card
-          img={Image1}
-          title="요가"
-          contents="요가에서 다양한 동작을 배워보아요"
-          tagtitle="요가"
-          footer="243명의 회원이 가입함"
-          onClick={() => navigate("/community/commuIn/1")}
-        />
-        <Card
-          img={Image2}
-          title="클라이밍 동아리"
-          contents="클라이밍에 재미를 붙이셨네요 :)"
-          tagtitle="클라이밍"
-          footer="200명의 회원이 가입함"
-          onClick={() => navigate("/community/commuIn/1")}
-        />
-      </Group>
-
-      <Group>
-        <Card
-          img={Image3}
-          title="필라테스를 사랑하는 모임"
-          contents="필라테스할 사람 모두 모두 모여라"
-          tagtitle="필라테스"
-          footer="194명의 회원이 가입함"
-          onClick={() => navigate("/community/commuIn/1")}
-        />
-        <Card
-          img={Image4}
-          title="축사모"
-          contents="축구에 진심이시군요"
-          tagtitle="축구"
-          footer="132명의 회원이 가입함"
-          onClick={() => navigate("/community/commuIn/1")}
-        />
-      </Group>
-    </>
+    <Group>
+      {(Array.isArray(popularCommunityData) 
+        ? popularCommunityData : [])
+        .slice(0, 4).map((community, index) => (
+          <div key={index} onClick={() => navigate(`/community/${community.communityNo}`)}>
+            <Card
+              img={community.profileImageUrl} 
+              title={community.communityName}
+              contents={community.intro}
+              footer={`${community.interest}명의 회원이 가입함`}
+            />
+          </div>
+        ))}
+    </Group>
   );
 };
 
