e3e6ed6933bfc9b1dff73a479216f3a375e0e01d
ahn <nameisb22@naver.com>
2023-10-04 17:29:05 +0900

    feat: 이미지 미리보기 기능

--- BodyShare-app/src/pages/mypage/myprofilemod/Profile.js
+++ BodyShare-app/src/pages/mypage/myprofilemod/Profile.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
+import React, { useState } from "react";
 
 const PicInput = styled.input`
+  margin-top: 15px;
   cursor: pointer;
 `
 
@@ -19,19 +21,44 @@ const ProfilePic = styled.button`
   background-color: #FFFFFF;
   color: #878787;
   font-size: 13px;
+  display: grid;
+  grid-template-columns: 1fr auto;
+`;
+
+const PreviewImage = styled.img`
+  max-width: 100%;
+  max-height: 60px; /* 세로 높이를 최대 60px로 제한 */
+  display: ${(props) => (props.src ? "block" : "none")};
 `;
 
 const Profile = function ({ register }) {
+  const [previewImage, setPreviewImage] = useState(null);
+
+  // 파일 선택 시 미리보기 이미지 업데이트
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setPreviewImage(e.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
   return (
     <>
       <TitleT>프로필 사진</TitleT>
       <ProfilePic>
-        <PicInput
-          {...register("profileImg")}
-          type="file"
-          accept="image/jpg, image/png, image/jpeg"
-        >
-        </PicInput>
+        <label htmlFor="profileImg">
+          <PicInput
+            {...register("profileImg")}
+            type="file"
+            accept="image/jpg, image/png, image/jpeg"
+            id="profileImg"
+            onChange={handleFileChange}
+          />
+        </label>
+        <PreviewImage src={previewImage} alt="Preview" />
       </ProfilePic>
     </>
   )
