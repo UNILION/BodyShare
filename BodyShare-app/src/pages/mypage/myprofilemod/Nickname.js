7e2850a0acf6430410299664ed2ddce01a6a232f
ahn <nameisb22@naver.com>
2023-10-04 11:22:41 +0900

    feat: 마이프로필 수정 닉네임 중복확인 기능

--- BodyShare-app/src/pages/mypage/myprofilemod/Nickname.js
+++ BodyShare-app/src/pages/mypage/myprofilemod/Nickname.js
@@ -1,4 +1,12 @@
 import styled from "styled-components";
+import { useRecoilValue } from "recoil";
+import axios from "axios";
+import { useState } from "react";
+
+const instance = axios.create({
+  baseURL: "http://localhost:33000/api",
+  withCredentials: true
+});
 
 const Titlel = styled.div`
   border-radius: 15px 0px 0px 15px;
@@ -11,6 +19,7 @@ const Titlel = styled.div`
 `
 
 const Input = styled.input`
+  width: 185px;
   height: 45px;
   color: #808080;
   border-radius: 0px 15px 15px 0px;
@@ -23,7 +32,7 @@ const Input = styled.input`
 const Ul = styled.ul`
   margin-top: 7px;
   display: grid;
-  grid-template-columns: 95px auto;
+  grid-template-columns: 95px 190px auto;
 `
 
 const Warn = styled.p`
@@ -37,25 +46,79 @@ const Warn = styled.p`
   width: 260px;
 `
 
-const Nickname = function ({nickname, register, errors}) {
+const NicknameButton = styled.button`
+  width: 68px;
+  height: 25px;
+  font-size: 13px;
+  color: white;
+  border-radius: 15px;
+  background-color: #556FFF;
+  border: none;
+  margin-left: 15px;
+  margin-top: 12px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Nickname = function ({ nickname, register, errors, getValues }) {
+
+  const [checkNic, setCheckNic] = useState("1");
+
+  const checkNicServer = async function (nic) {
+    try {
+      if (!nic) {
+        // 닉네임이 비어 있는 경우 중복 확인을 하지 않음
+        return;
+      }
+      const response = await instance.post('/users/checknic', { nic });
+      if (response.data.check) {
+        //중복 있음
+        setCheckNic("2");
+      } else {
+        //중복 없음
+        setCheckNic("3");
+      }
+    } catch (error) {
+      // 에러 처리
+      console.error(error);
+    }
+  };
+
+  const checkNicValidate = function () {
+    if (checkNic == 1) {
+      return "중복확인을 해주세요";
+    }
+    else if (checkNic == 2) {
+      return "중복된 닉네임입니다."
+    }
+    else {
+      return;
+    }
+  };
+
   return (
     <>
       <Ul>
         <Titlel>닉네임</Titlel>
-        <Input 
-        {...register("nickname", {
-          required: "닉네임을 적어주세요",
-          maxLength: {
-            value: 10,
-            message: "최대 10글자까지 가능합니다",
-          },
-        })}
-        placeholder={nickname}
-        defaultValue={nickname}
+        <Input
+          {...register("nickname", {
+            required: '닉네임을 입력하세요',
+            pattern: {
+              value: /^[a-zA-Z0-9가-힣]{4,16}$/,
+              message: '영문, 한글, 숫자로 이루어진 4~16자',
+            },
+            validate: checkNicValidate,
+          })}
+          placeholder={nickname}
         >
         </Input>
+        <NicknameButton onClick={() => checkNicServer(getValues('nickname'))}>중복확인</NicknameButton>
       </Ul>
-      <Warn>{errors.nickname?.message}</Warn>
+      {errors.nickname && (
+        <Warn>{errors.nickname.message}</Warn>
+      )}
     </>
   )
 };
