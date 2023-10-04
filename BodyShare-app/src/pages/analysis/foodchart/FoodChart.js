a9478bf8bf3c66ad4d9e355e6b4aa61547fd4b08
LAPTOP-NTEF1TGQ\tlstj <tlstj212@naver.com>
2023-10-04 12:29:46 +0900

    feat: food차트 두번쨰 슬라이드 부분 db 연동

--- BodyShare-app/src/pages/analysis/foodchart/FoodChart.js
+++ BodyShare-app/src/pages/analysis/foodchart/FoodChart.js
@@ -9,6 +9,8 @@ import { useRecoilValue } from "recoil";
 import { userSelector } from "recoil/userRecoil";
 import { foodSelector } from "recoil/foodList";
 import Slide1 from "./Slide1";
+import Slide2 from "./Slide2";
+
 
 const instance = axios.create({
   baseURL: "http://localhost:33000/api",
@@ -62,10 +64,6 @@ const ChartContainer = styled.div`
 `;
 
 const FoodChart = function () {
-  const userNo = useRecoilValue(userSelector);
-  const [selectedFood] = useRecoilValue(foodSelector);
-  const [foodChartData, setFoodChartData] = useState([]);
-
   const parseDateString = (dateString) => {
     const dateParts = dateString.split(".");
     const year = parseInt(dateParts[0],10);
@@ -75,67 +73,6 @@ const FoodChart = function () {
   }
 
 
-
-
-
-// 요일별 칼로리 
-// const chartDatas2 = async function () {
-//   try {
-//     const response = await instance.get(`/record/food/${userNo}`);
-//     const allData = response.data;
-
-//     const currentDate = new Date();
-//     const currentWeekStartDate = new Date(currentDate);
-//     currentWeekStartDate.setDate(
-//       currentDate.getDate() - currentDate.getDay()
-//     );
-//     const currentWeekEndDate = new Date(currentWeekStartDate);
-//     currentWeekEndDate.setDate(currentWeekStartDate.getDate() + 6);
-
-//     let totalCalories = 0; // 주간 칼로리 누적값을 저장할 변수
-
-//     for (let i = 0; i < 7; i++) {
-//       const day = currentWeekStartDate.getDate() + i; // 현재 주의 시작일부터 7일 동안
-//       const foodItem = allData.find((record) => {
-//         const recordDate = new Date(record.dietDate);
-//         return recordDate.getDate() === day;
-//       });
-
-//         totalCalories += foodItem.calories;
-//     }
-
-//     // 주간 칼로리 정보를 그래프에 추가
-//     const foodChartData = [
-//       ["요일", "주간 칼로리"],
-//       ["일", 0],
-//       ["월", 0],
-//       ["화", 0],
-//       ["수", 0],
-//       ["목", 0],
-//       ["금", 0],
-//       ["토", 0],
-//     ];
-
-//     foodChartData[1][1] = totalCalories; // 해당 요일에 주간 칼로리 누적값 설정
-
-//     setFoodChartData(foodChartData);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-//   useEffect(() => {
-//     chartDatas2();
-//     const interval = setInterval(() => {
-//       chartDatas2(); 
-//     }, 24 * 60 * 60 * 1000); 
-
-//     return () => clearInterval(interval);
-//   }, [userNo, selectedFood]);
-
-  const chartOptions2 = {
-    title: "요일별 칼로리 섭취량",
-    curveType: "function",
-  };
 
   // 슬라이드
   const settings = {
@@ -148,20 +85,7 @@ const FoodChart = function () {
       <Slider {...settings}>
         <Slide1 />
 
-        <Slide>
-          <ChartBox>
-            <ChartContainer>
-              <Chart
-                chartType="ComboChart"
-                width="340px"
-                height="456px"
-                data={foodChartData}
-                options={chartOptions2}
-                graph_id="curve_chart"
-              />
-            </ChartContainer>
-          </ChartBox>
-        </Slide>
+        <Slide2 />
       </Slider>
     </SliderContainer>
   );
--- BodyShare-app/src/pages/analysis/foodchart/Slide1.js
+++ BodyShare-app/src/pages/analysis/foodchart/Slide1.js
@@ -4,7 +4,6 @@ import { Chart } from "react-google-charts";
 import axios from "axios";
 import { useRecoilValue } from "recoil";
 import { userSelector } from "recoil/userRecoil";
-import { foodSelector } from "recoil/foodList";
 
 const instance = axios.create({
   baseURL: "http://localhost:33000/api",
@@ -12,25 +11,17 @@ const instance = axios.create({
 }); 
 
 const Slide = styled.div`
-  min-width: 100%;
+  width: 360px;
   display: grid;
   place-items: center;
   transition: transform 0.3s ease;
   grid-column: span 1;
-`;
-
-const ChartBox = styled.div`
-  width: 360px;
-  height: 476px;
   border-radius: 30px;
   background-color: #ffffff;
   border: 1px solid rgba(0, 0, 0, 0.25);
   margin: 0 auto;
   box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
   cursor: pointer;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 `;
 
 const ChartContainer = styled.div`
@@ -88,14 +79,6 @@ const Slide1 = function () {
       console.error(error);
     }
   };
-
-  const intervalTime = function () {
-    const interval = setInterval(() => {
-      chartDatas1(); 
-    }, 24 * 60 * 60 * 1000); 
-    return () => clearInterval(interval);
-  }
-
  
   useEffect(() => {
     chartDatas1();
@@ -103,21 +86,19 @@ const Slide1 = function () {
 
   return(
     <Slide>
-    <ChartBox>
       <ChartContainer>
       <Chart
           chartType="PieChart"
-          width="350px"
-          height="240px"
+          width={"340px"}
+          height={"450px"}
           data={foodChartData}
           options={{
             title: "Calorie",
-            pieHole: 0.4,
+            pieHole: 0.2,
           }}
-          graph_id="foodchart1"
+          graph_id="donutchart"
         />
       </ChartContainer>
-    </ChartBox>
   </Slide>
   );
 }
