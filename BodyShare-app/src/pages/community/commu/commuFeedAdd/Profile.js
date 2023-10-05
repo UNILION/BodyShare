651f2c0811c6ea14cfd995b11488346c4ace28e8
ahn <nameisb22@naver.com>
2023-10-05 13:15:32 +0900

    refactor: 커뮤니티 피드 추가 페이지 이미지 업로드 미리보기 css 변경

--- BodyShare-app/src/pages/community/commu/commuFeedAdd/Profile.js
+++ BodyShare-app/src/pages/community/commu/commuFeedAdd/Profile.js
@@ -2,6 +2,11 @@ import styled from "styled-components";
 
 const ProfileContainer = styled.div``;
 
+const Top = styled.div`
+display: grid;
+grid-template-columns: 1.5fr 1fr;
+`
+
 const Button = styled.div`
 margin-bottom : 10px;
 background-color: #556FFF;
@@ -10,10 +15,11 @@ border: 1px solid rgba(135,135,135,0.3);
 border-radius: 15px;
 font-size: 20px;
 width: 150px;
-height: 35px;
+height: 30px;
 text-align: center;
 line-height: 35px;
 cursor: pointer;
+margin-top: 5px;
 `;
 
 const Title = styled.div`
@@ -31,21 +37,23 @@ const Picture = styled.div`
 `;
 
 const Profile = function ({ register, imagePreview, handleImageChange }) {
-  
+
   return (
     <ProfileContainer>
-      <Title>피드 사진 추가하기</Title>
+      <Top>
+        <Title>피드 사진 추가하기</Title>
         <Button type="text"><label htmlFor="imgUpload" Style="cursor: pointer;">이미지 추가하기</label></Button>
+      </Top>
       <Picture>
         <input
           {...register("contentImageUrl")}
           type="file"
           accept="image/jpg, image/png, image/jpeg"
-          id = "imgUpload"
-          style={{display:"none"}}
+          id="imgUpload"
+          style={{ display: "none" }}
           onChange={handleImageChange}
         />
-        {imagePreview ? <img src={imagePreview} style={{width:"100px", height: "100px"}}/> : null}
+        {imagePreview ? <img src={imagePreview} style={{ width: "100px", height: "100px" }} /> : null}
       </Picture>
     </ProfileContainer>
   );
