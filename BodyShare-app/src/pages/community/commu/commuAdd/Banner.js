13f7ca499dd7d320b3d690265aa93cfaef0eaa49
ahn <nameisb22@naver.com>
2023-10-05 13:14:32 +0900

    feat: 커뮤니티 추가 페이지 배너 이미지 업로드 미리보기 추가

--- BodyShare-app/src/pages/community/commu/commuAdd/Banner.js
+++ BodyShare-app/src/pages/community/commu/commuAdd/Banner.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import React, { useState } from "react";
 
 const BannerContainer = styled.div``;
 
@@ -16,20 +17,52 @@ const Picture = styled.div`
   text-align: center;
   padding-left: 50px;
   line-height: 60px;
+  display: grid;
+  grid-template-columns: 1fr auto;
 `;
 
-const Banner = function ({register}) {
-  return(
+const PicInput = styled.input`
+  margin-top: 15px;
+  cursor: pointer;
+`;
+
+const PreviewImage = styled.img`
+  max-width: 100%;
+  max-height: 60px; /* 세로 높이를 최대 60px로 제한 */
+  display: ${(props) => (props.src ? "block" : "none")};
+`;
+
+const Banner = function ({ register }) {
+  const [previewImage, setPreviewImage] = useState(null);
+
+  // 파일 선택 시 미리보기 이미지 업데이트
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setPreviewImage(e.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
     <BannerContainer>
-    <Title>배너 사진</Title>
-    <Picture>
-      <input
-        {...register("bannerImg")}
-        type="file"
-        accept="image/jpg, image/png, image/jpeg"
-      />
-    </Picture>
-  </BannerContainer>
+      <Title>배너 사진</Title>
+      <Picture>
+        <label htmlFor="bannerImg">
+          <PicInput
+            {...register("bannerImg")}
+            type="file"
+            accept="image/jpg, image/png, image/jpeg"
+            id="bannerImg"
+            onChange={handleFileChange}
+          />
+        </label>
+        <PreviewImage src={previewImage} alt="Preview" />
+      </Picture>
+    </BannerContainer>
   )
 }
 
