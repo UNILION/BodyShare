58165113e3d297178e11b453f12721e922d5f22d
ahn <nameisb22@naver.com>
2023-10-05 13:14:46 +0900

    feat: 커뮤니티 추가 페이지 프로필 이미지 업로드 미리보기 추가

--- BodyShare-app/src/pages/community/commu/commuAdd/Profile.js
+++ BodyShare-app/src/pages/community/commu/commuAdd/Profile.js
@@ -1,6 +1,12 @@
 import styled from "styled-components";
+import React, { useState } from "react";
 
 const ProfileContainer = styled.div``;
+
+const PicInput = styled.input`
+  margin-top: 15px;
+  cursor: pointer;
+`
 
 const Title = styled.div`
   font-weight: bold;
@@ -16,19 +22,47 @@ const Picture = styled.div`
   text-align: center;
   padding-left: 50px;
   line-height: 60px;
+  display: grid;
+  grid-template-columns: 1fr auto;
 `;
 
+const PreviewImage = styled.img`
+  max-width: 100%;
+  max-height: 60px; /* 세로 높이를 최대 60px로 제한 */
+  display: ${(props) => (props.src ? "block" : "none")};
+`;
+
+
+
 const Profile = function ({ register }) {
+  const [previewImage, setPreviewImage] = useState(null);
+
+  // 파일 선택 시 미리보기 이미지 업데이트
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setPreviewImage(e.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
   return (
     <ProfileContainer>
       <Title>프로필 사진</Title>
       <Picture>
-        <input
-          {...register("profileImg")}
-          type="file"
-          accept="image/jpg, image/png, image/jpeg"
-        />
-      </Picture>
+        <label htmlFor="profileImg">
+          <PicInput
+            {...register("profileImg")}
+            type="file"
+            accept="image/jpg, image/png, image/jpeg"
+            id="profileImg"
+            onChange={handleFileChange}
+          />
+        </label>
+        <PreviewImage src={previewImage} alt="Preview" />
+      </Picture> 
     </ProfileContainer>
   );
 };
