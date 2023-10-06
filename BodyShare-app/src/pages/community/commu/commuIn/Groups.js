e47441307752704a41ee3d7c27be21296f8767fd
runkey <csi9509@naver.com>
2023-10-07 03:16:59 +0900

    feat: 커뮤니티 내부 피드 없을 시 처리

--- BodyShare-app/src/pages/community/commu/commuIn/Groups.js
+++ BodyShare-app/src/pages/community/commu/commuIn/Groups.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import Card from "components/commons/Card";
-import {  useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Group = styled.div`
   display: grid;
@@ -11,24 +11,34 @@ const Group = styled.div`
   overflow-x: hidden;
 `;
 
-const Groups = function ({groupLists}) {
+const Not = styled.div`
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold;
+  width: 310px;
+  height: 290px;
+  margin-top: 30px;
+  overflow-y: hidden;
+`
+
+const Groups = function ({ groupLists }) {
   const navigate = useNavigate();
 
 
   return (
     <>
       <Group>
-        {groupLists.map((data, idx) => (
-      <Card
-          key = {idx}
-          img={`http://localhost:33000/images/posts/${data.contentImageUrl}`}
-          title={data.title}
-          contents={data.content}
-          footer={String(data.createdDate.split('-')[0]) + "년" + String(data.createdDate.split('-')[1]) + "월" + String(data.createdDate.split('-')[2]).substring(0,2) + "일"}
-          onClick={() => navigate(`/community/feed/${data.postNo}`)}
-          mb="10px"
-        />
-        ))}
+        {groupLists.length > 0 ? groupLists.map((data, idx) => (
+          <Card
+            key={idx}
+            img={`http://localhost:33000/images/posts/${data.contentImageUrl}`}
+            title={data.title}
+            contents={data.content}
+            footer={String(data.createdDate.split('-')[0]) + "년" + String(data.createdDate.split('-')[1]) + "월" + String(data.createdDate.split('-')[2]).substring(0, 2) + "일"}
+            onClick={() => navigate(`/community/feed/${data.postNo}`)}
+            mb="10px"
+          />
+        )) : <Not>커뮤니티 내부에 피드가 존재하지 않습니다.</Not>}
       </Group>
     </>
   );
