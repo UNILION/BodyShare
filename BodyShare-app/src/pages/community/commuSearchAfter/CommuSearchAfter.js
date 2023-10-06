d6ccfd2bbec84149e82937c9671d5cda305fdfb8
runkey <csi9509@naver.com>
2023-10-07 03:07:32 +0900

    feat: 커뮤니티 검색 후 화면, 커뮤니티 추가 버튼 이동

--- BodyShare-app/src/pages/community/commuSearchAfter/CommuSearchAfter.js
+++ BodyShare-app/src/pages/community/commuSearchAfter/CommuSearchAfter.js
@@ -1,20 +1,34 @@
 import styled from "styled-components";
 import Top from "./Top";
 import Middle from "./Middle";
+import Plus from "assets/Img/buttonplus.png";
 import { useRecoilValue } from "recoil";
 import { searchSelector } from "recoil/commuRecoil";
+import { useNavigate } from "react-router-dom";
 
 const Container = styled.div`
   padding: 20px;
 `;
 
+const AddImg = styled.img`
+  display: grid;
+  align-items: end;
+  margin-left: auto;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  margin-top: 20px;
+`;
+
 const CommuSearchAfter = function () {
   const searchItemList = useRecoilValue(searchSelector);
+  const navigate = useNavigate();
 
   return (
     <Container>
       <Top searchItemList={searchItemList}/>
       <Middle searchItemList={searchItemList}/>
+      <AddImg src={Plus} onClick={() => navigate("/community/communityAdd")} />
     </Container>
   );
 };
