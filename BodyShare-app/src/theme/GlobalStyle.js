5bb67fc0f9dfdc028abfe5859aa5d23e305c7cf6
runkey <csi9509@naver.com>
2023-10-06 19:37:58 +0900

    style: 스크롤바 디자인 수정 및 전체 적용

--- BodyShare-app/src/theme/GlobalStyle.js
+++ BodyShare-app/src/theme/GlobalStyle.js
@@ -3,9 +3,31 @@ import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
+   /* Width */
+   ::-webkit-scrollbar {
+    width: 12px;
+  }
+
+  /* Track */
+  ::-webkit-scrollbar-track {
+    background: #f1f1f1;
+  }
+
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    background: rgba(85, 111, 255, 0.7);
+    border-radius: 6px;
+  }
+
+  /* Handle on hover */
+  ::-webkit-scrollbar-thumb:hover {
+    background: #556fff;
+  }
+
   body {
     background: ${({ theme }) => theme.bgColor};
     color: ${({ theme }) => theme.textColor};
+    
     @font-face {
       font-family: 'GmarketSansMedium';
       src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
