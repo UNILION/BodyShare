b2b6763f024ffcae3141a5d2e00f78bccad4b4bb
runkey <csi9509@naver.com>
2023-10-08 20:28:36 +0900

    style : 커뮤니티 검색 화면 리스트 다크모드 색 적용

--- BodyShare-app/src/pages/community/commu/commuSearch/SearchListItem.js
+++ BodyShare-app/src/pages/community/commu/commuSearch/SearchListItem.js
@@ -1,23 +1,26 @@
 import { useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
+import { isDarkAtom } from "recoil/themeRecoil";
 import styled from "styled-components";
 
 const SportSearchResult = styled.div`
+  margin-left : 8px;
   margin-bottom: 20px;
   justify-content: center;
 `;
 
 const ResultButton = styled.button`
   width: 360px;
-  //margin-top: 10px;
-  background-color: ${(props) => (props.active ? props.hovercolor : 'white')};
+  background-color: ${(props) => (props.isDarkMode ? (props.active ? props.hovercolor : 'black') : (props.active ? props.hovercolor : 'white'))};
   border: none;
   border-radius: 15px;
   padding-top: 25px;
   cursor: pointer;
   transition: background-color 0.2s; 
+  color: ${(props) => (props.isDarkMode ? 'white' : 'black')};
 
   &:hover {
-    background-color: ${(props) => (props.active ? props.hovercolor : 'white')};
+    background-color:${(props) => (props.isDarkMode ? (props.active ? props.hovercolor : 'black') : (props.active ? props.hovercolor : 'white'))};
   }
 `;
 
@@ -25,46 +28,54 @@ const RP = styled.p`
   font-size: 17px;
   text-align: left;
   margin-bottom: 10px;
+  color: ${(props) => (props.color ? props.color : 'black')};
 `;
 
 const Line = styled.div`
   width: 340px;
-  border: 1px solid rgba(135, 135, 135, 0.3);
+  border: ${(props) => (props.isDarkMode ? "1px solid rgba(255, 255, 255, 0.7)": '1px solid rgba(135, 135, 135, 0.3)')};
 `;
 
-const SearchListItem = function ( { record, selectedList, changeSelected } ) {
+const Cover = styled.div`
+  background-color: white;
+  border-radius: 15px;
+  width:360px;
+`
+
+const SearchListItem = function ({ record, selectedList, changeSelected }) {
   const [buttonActive, setButtonActive] = useState(false);
+  const isDarkMode = useRecoilValue(isDarkAtom);
 
   const handleButtonClick = () => {
-    if(selectedList.length == 0){
-      if(buttonActive == false){
+    if (selectedList.length == 0) {
+      if (buttonActive == false) {
         changeSelected([...selectedList, record])
-      } else{
-        const temp = [...selectedList].filter(item => item.no != record.no );
+      } else {
+        const temp = [...selectedList].filter(item => item.no != record.no);
         changeSelected(temp);
       }
       setButtonActive(!buttonActive);
-    }else if(selectedList.length == 1){
-      if(buttonActive == true){
-        const temp = [...selectedList].filter(item => item.no != record.no );
+    } else if (selectedList.length == 1) {
+      if (buttonActive == true) {
+        const temp = [...selectedList].filter(item => item.no != record.no);
         changeSelected(temp);
         setButtonActive(!buttonActive);
       }
     }
   };
 
-  const detect = function(){
-    if(!buttonActive){
-      const temp = [...selectedList].filter(item => item.no == record.no );
-      if(temp.length != 0){
+  const detect = function () {
+    if (!buttonActive) {
+      const temp = [...selectedList].filter(item => item.no == record.no);
+      if (temp.length != 0) {
         setButtonActive(!buttonActive);
       }
-    }else if(buttonActive){
-      const temp = [...selectedList].filter(item => item.no == record.no );
-      if(temp.length == 0){
+    } else if (buttonActive) {
+      const temp = [...selectedList].filter(item => item.no == record.no);
+      if (temp.length == 0) {
         setButtonActive(!buttonActive);
       }
-    }  
+    }
   };
 
   useEffect(() => {
@@ -73,14 +84,17 @@ const SearchListItem = function ( { record, selectedList, changeSelected } ) {
 
   return (
     <SportSearchResult>
+    <Cover>
       <ResultButton
         active={buttonActive}
         onClick={handleButtonClick}
+        isDarkMode={isDarkMode}
         hovercolor="rgba(85, 111, 255, 0.7)"
       >
-        <RP>{record.name}</RP>
-        <Line></Line>
+        <RP color={isDarkMode ? "white" : "black"}>{record.name}</RP>
+        <Line isDarkMode={isDarkMode} />
       </ResultButton>
+        </Cover>
     </SportSearchResult>
   );
 };
