a2257725c53af28618c2ba458e9455f51d151463
runkey <csi9509@naver.com>
2023-10-11 03:51:25 +0900

    feat: postNo 기준으로 기록 날짜 불러오기

--- api-server/models/recordModel.js
+++ api-server/models/recordModel.js
@@ -30,17 +30,17 @@ const recordModel = {
   },
 
   // 해당 유저의 당일 운동 기록 목록 조회
-  async findByRecentToday(no, day) {
+  async findByRecentToday(no, postNo) {
     try {
       const sql = `SELECT er.*, s.name AS sportsName,
-        (select count(*) from exerciseRecord er where er.userNo = ? and DATE(STR_TO_DATE(er.exerciseDate, '%Y. %m. %d.')) = DATE(STR_TO_DATE(?, '%Y. %m. %d.'))) as cnt_exercise,
-        (select sum(exerciseTime) from exerciseRecord er where er.userNo = ? and DATE(STR_TO_DATE(er.exerciseDate, '%Y. %m. %d.')) = DATE(STR_TO_DATE(?, '%Y. %m. %d.'))) as total_time
-        FROM exerciseRecord er
-        INNER JOIN sports s ON er.sportsNo = s.no
-        WHERE er.userNo = ?
-        AND DATE(STR_TO_DATE(er.exerciseDate, '%Y. %m. %d.')) = DATE(STR_TO_DATE(?, '%Y. %m. %d.'))
-        ORDER BY er.exerciseDate DESC;`;
-      const [result] = await pool.query(sql, [no, day, no, day, no, day]);
+      (select count(*) from exerciseRecord er where er.userNo = ? and DATE(STR_TO_DATE(er.exerciseDate, '%Y. %m. %d.')) = DATE(STR_TO_DATE((SELECT recordDate FROM communityPost where postNo=?), '%Y. %m. %d.'))) as cnt_exercise,
+      (select sum(exerciseTime) from exerciseRecord er where er.userNo = ? and DATE(STR_TO_DATE(er.exerciseDate, '%Y. %m. %d.')) = DATE(STR_TO_DATE((SELECT recordDate FROM communityPost where postNo=?), '%Y. %m. %d.'))) as total_time
+      FROM exerciseRecord er
+      INNER JOIN sports s ON er.sportsNo = s.no
+      WHERE er.userNo = ?
+      AND DATE(STR_TO_DATE(er.exerciseDate, '%Y. %m. %d.')) = DATE(STR_TO_DATE((SELECT recordDate FROM communityPost where postNo=?), '%Y. %m. %d.'))
+      ORDER BY er.exerciseDate DESC;`;
+      const [result] = await pool.query(sql, [no, postNo, no, postNo, no, postNo]);
       return result;
     } catch (err) {
       throw new Error("DB Error", { cause: err });
