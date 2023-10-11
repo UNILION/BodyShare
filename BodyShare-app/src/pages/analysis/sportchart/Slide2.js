0a01b4530b8652e8eaa3519fd1d50777e6c0e5b1
LAPTOP-NTEF1TGQ\tlstj <tlstj212@naver.com>
2023-10-11 15:49:17 +0900

    fix:총칼로리 계산 오류 수정

--- BodyShare-app/src/pages/analysis/sportchart/Slide2.js
+++ BodyShare-app/src/pages/analysis/sportchart/Slide2.js
@@ -41,7 +41,7 @@ const Slide2 = function () {
   const userNo = useRecoilValue(userSelector);
   const [chartData, setChartData] = useState([]);
   const [sum, setSum] = useState(0);
-  
+
   const chartDatas = async function () {
     try {
       const exerciseResponse = await instance.get(`/record/sports/${userNo}`);
@@ -50,15 +50,12 @@ const Slide2 = function () {
       const exerciseData = exerciseResponse.data;
       const foodData = foodResponse.data;
 
-      const currentDate = new Date(); // 현재
-      const currentWeekStartDate = new Date(currentDate); // 시작
-      currentWeekStartDate.setDate(
-        currentDate.getDate() - currentDate.getDay()
-      );
+      const currentDate = new Date();
+      const currentWeekStartDate = new Date(currentDate);
+      currentWeekStartDate.setDate(currentDate.getDate() - currentDate.getDay());
       const currentWeekEndDate = new Date(currentWeekStartDate);
       currentWeekEndDate.setDate(currentWeekStartDate.getDate() + 6);
 
-      // 현재 주 해당 칼로리기록만 저장
       const currentWeekData = foodData.filter((record) => {
         const recordDate = new Date(record.dietDate);
         return (
@@ -68,23 +65,22 @@ const Slide2 = function () {
 
       const chartData = [["일자", "소모 칼로리", "섭취 칼로리"]];
       const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
-      let temp = sum
+      let temp = 0; // Initialize temp to zero
+
       for (let i = 0; i < 7; i++) {
-        // 데이터 추가
         const day = daysOfWeek[i];
         const date = new Date(currentWeekStartDate);
         date.setDate(currentWeekStartDate.getDate() + i);
         const options = {
-          // 2자리표현
           month: "2-digit",
           day: "2-digit",
         };
-        const formattedDate = date.toLocaleDateString("ko-KR", options); // 월/일 (요일)
+        const formattedDate = date.toLocaleDateString("ko-KR", options);
         const dateString = `${formattedDate}\n(${day})`;
 
         let Totalconsum = 0;
+        let consumedCalories = 0;
 
-        // 해당 날짜에 대한 운동 기록 더하기
         exerciseData.forEach((record) => {
           const recordDate = new Date(record.exerciseDate);
           const recordDateString = recordDate.toLocaleDateString("ko-KR", options);
@@ -94,20 +90,18 @@ const Slide2 = function () {
           }
         });
 
-        const consumedCaloriesRecord = currentWeekData.find((record) => {
-          // 현재 요일에 해당되는 음식 칼로리 기록이 있는지 확인
+        currentWeekData.forEach((record) => {
           const recordDate = new Date(record.dietDate);
-          return recordDate.getDay() === i;
+          if (recordDate.getDay() === i) {
+            consumedCalories += record.calories;
+          }
         });
 
-        const consumedCalories = consumedCaloriesRecord
-          ? consumedCaloriesRecord.calories
-          : 0;
-        temp += Totalconsum + consumedCalories
         chartData.push([dateString, Totalconsum, consumedCalories]);
+        temp += consumedCalories;
       }
 
-      setSum(temp)
+      setSum(temp);
       setChartData(chartData);
     } catch (error) {
       console.log(error);
@@ -117,14 +111,14 @@ const Slide2 = function () {
   useEffect(() => {
     chartDatas();
     const interval = setInterval(() => {
-      chartDatas(); // 일정 간격으로 데이터 갱신
-    }, 24 * 60 * 60 * 1000); // 매일 한 번 갱신 (24시간 간격)
+      chartDatas();
+    }, 24 * 60 * 60 * 1000);
 
     return () => clearInterval(interval);
   }, [userNo]);
 
   const chartOptions = {
-    title: "소모 칼로리와 섭취 칼로리 비교",
+    title: "운동 시간과 섭취 칼로리 비교",
     legend: { position: "top" },
     bars: "horizontal",
     titleTextStyle: {
