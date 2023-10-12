77753626c803732e3bd7087234126721eaf29ce3
Dongyun <kj77181@gmail.com>
2023-10-12 10:39:32 +0900

    feat: npm i dotenv 및 코드 기능 추가

--- api-server/config/index.js
+++ api-server/config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
   mysql: {
-    host: "13.124.253.233",
-    port: "3306",
-    database: "bodyshare",
-    user: "bodyshare",
-    password: "Bodyshare12$$",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     timezone: "-09:00",
   }
 };
